b20221e77bebbd2a5f1894fc4f8839eb96349f27
shenghe <sheng.he@majorbio.com>
2015-12-01 17:19:12 +0800

    添加了beta分析module

---
+++ src/mbio/modules/meta/beta_diversity/beta_diversity.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+# __author__ = 'shenghe'
+
+from biocluster.module import Module
+import os
+from biocluster.core.exceptions import OptionError
+
+
+class BetaDiversityModule(Module):
+
+    def __init__(self, work_id):
+        super(BetaDiversityModule, self).__init__(work_id)
+        self.logger.info('init1')
+        options = [
+            {"name": "analysis", "type": "string",
+                "default": "anosim,pca,pcoa,nmds,rda_cca,dbrda,hcluster"},
+            {"name": "dis_method", "type": "string", "default": "bray_curtis"},
+            {"name": "otutable", "type": "infile", "format": "meta.otu.otu_table"},
+            {"name": "phy_newick", "type": "infile",
+             "format": "meta.beta_diversity.newick_tree"},
+            {"name": "permutations", "type": "int", "default": 999},
+            {"name": "linkage", "type": "string", "default": "average"},
+            {"name": "envtable", "type": "infile", "format": "meta.env_table"},
+            {"name": "group", "type": "infile", "format": "meta.otu.group_table"}
+        ]
+        self.add_option(options)
+        self.matrix = self.add_tool('meta.beta_diversity.distance_calc')
+        self.tools = {}
+        self.logger.info('init2')
+
+    def check_options(self):
+        self.logger.info('check1')
+        if self.option('permutations') < 0 or self.option('permutations') > 10000:
+            raise OptionError('参数permutations：%s 不在范围内(0-10000)' %
+                              self.option('permutations'))
+        samplelist = open(self.option('otutable').prop[
+                          'path']).readline().strip().split('\t')[1:]
+        for sample in self.option('group').prop['sample']:
+            if sample not in samplelist:
+                raise OptionError('分组文件的样本(%s)在otu表的样本中不存在' % sample)
+        if self.option('linkage') not in ['average', 'single', 'complete']:
+            raise OptionError('错误的层级聚类方式：%s' % self.option('linkage'))
+        if 'rda_cca' in self.option('analysis') and not self.option('envtable').is_set:
+            raise OptionError('计算RDA/CCA需要环境因子表')
+        if ('anosim' or 'rda_cca' or 'dbrda') in self.option('analysis') and not self.option('group').is_set:
+            raise OptionError('分析需要相关分组文件')
+        self.logger.info('check2')
+
+    def matrix_run(self):
+        """
+        运行计算距离矩阵
+        :return:
+        """
+        if self.option('phy_newick').is_set:
+            self.matrix.set_options({'method': self.option('dis_method'),
+                                     'otutable': self.option('otutable').prop['path'],
+                                     'newicktree': self.option('phy_newick').prop['path']})
+        else:
+            self.matrix.set_options({'method': self.option('dis_method'),
+                                     'otutable': self.option('otutable').prop['path']})
+        self.matrix.on('end', self.set_output, 'distance')
+        self.matrix.run()
+
+    def hcluster_run(self, rely_obj):
+        output_file_obj = rely_obj.rely[0].option('dis_matrix')
+        self.tools['hcluster'].set_options({
+            'dis_matrix': output_file_obj.prop['path'],
+            'linkage': self.option('linkage')
+        })
+        self.tools['hcluster'].on('end', self.set_output, 'hcluster')
+        self.tools['hcluster'].run()
+
+    def anosim_run(self, rely_obj):
+        output_file_obj = rely_obj.rely[0].option('dis_matrix')
+        self.tools['anosim'].set_options({
+            'dis_matrix': output_file_obj.prop['path'],
+            'group': self.option('group').prop['path'],
+            'permutations': self.option('permutations')
+        })
+        self.tools['anosim'].on('end', self.set_output, 'anosim')
+        self.tools['anosim'].run()
+
+    def box_run(self, rely_obj):
+        output_file_obj = rely_obj.rely[0].option('dis_matrix')
+        self.tools['box'].set_options({
+            'dis_matrix': output_file_obj.prop['path'],
+            'group': self.option('group').prop['path']
+        })
+        self.tools['box'].on('end', self.set_output, 'box')
+        self.tools['box'].run()
+
+    def pcoa_run(self, rely_obj):
+        output_file_obj = rely_obj.rely[0].option('dis_matrix')
+        self.tools['pcoa'].set_options({
+            'dis_matrix': output_file_obj.prop['path']
+        })
+        self.tools['pcoa'].on('end', self.set_output, 'pcoa')
+        self.tools['pcoa'].run()
+
+    def nmds_run(self, rely_obj):
+        output_file_obj = rely_obj.rely[0].option('dis_matrix')
+        self.tools['nmds'].set_options({
+            'dis_matrix': output_file_obj.prop['path']
+        })
+        self.tools['nmds'].on('end', self.set_output, 'nmds')
+        self.tools['nmds'].run()
+
+    def dbrda_run(self, rely_obj):
+        output_file_obj = rely_obj.rely[0].option('dis_matrix')
+        self.tools['dbrda'].set_options({
+            'dis_matrix': output_file_obj.prop['path'],
+            'group': self.option('group').prop['path']
+        })
+        self.tools['dbrda'].on('end', self.set_output, 'dbrda')
+        self.tools['dbrda'].run()
+
+    def rda_run(self):
+        self.tools['rda'].set_options({
+            'otutable': self.option('otutable').prop['path'],
+            'envtable': self.option('envtable').prop['path']
+        })
+        self.tools['rda'].on('end', self.set_output, 'rda')
+        self.tools['rda'].run()
+
+    def pca_run(self):
+        if self.option('envtable').is_set:
+            self.tools['pca'].set_options({
+                'otutable': self.option('otutable').prop['path'],
+                'envtable': self.option('envtable').prop['path']})
+        else:
+            self.tools['pca'].set_options({
+                'otutable': self.option('otutable').prop['path']})
+        self.tools['pca'].on('end', self.set_output, 'pca')
+        self.tools['pca'].run()
+
+    def set_output(self, event):
+        obj = event['bind_object']
+        if event['data'] == 'pca':
+            self.linkdir(obj.output_dir, 'Pca')
+        elif event['data'] == 'rda':
+            self.linkdir(obj.output_dir, 'Rda')
+        elif event['data'] == 'distance':
+            self.linkdir(obj.output_dir, 'Distance')
+        elif event['data'] == 'hcluster':
+            self.linkdir(obj.output_dir, 'Hcluster')
+        elif event['data'] == 'anosim':
+            self.linkdir(obj.output_dir, 'Anosim')
+        elif event['data'] == 'box':
+            self.linkdir(obj.output_dir, 'Box')
+        elif event['data'] == 'dbrda':
+            self.linkdir(obj.output_dir, 'Dbrda')
+        elif event['data'] == 'pcoa':
+            self.linkdir(obj.output_dir, 'Pcoa')
+        elif event['data'] == 'nmds':
+            self.linkdir(obj.output_dir, 'Nmds')
+        else:
+            pass
+
+    def linkdir(self, dirpath, dirname):
+        """
+        link一个文件夹下的所有文件到本module的output目录
+        :param dirpath: 传入文件夹路径
+        :param dirname: 新的文件夹名称
+        :return:
+        """
+        allfiles = os.listdir(dirpath)
+        newdir = os.path.join(self.output_dir, dirname)
+        if not os.path.exists(newdir):
+            os.mkdir(newdir)
+        oldfiles = [os.path.join(dirpath, i) for i in allfiles]
+        newfiles = [os.path.join(newdir, i) for i in allfiles]
+        for newfile in newfiles:
+            if os.path.exists(newfile):
+                os.remove(newfile)
+        for i in range(len(allfiles)):
+            os.link(oldfiles[i], newfiles[i])
+
+    def run(self):
+        super(BetaDiversityModule, self).run()
+        if 'anosim' in self.option('analysis'):
+            self.tools['anosim'] = self.add_tool('meta.beta_diversity.anosim')
+            self.on_rely(self.matrix, self.anosim_run)
+            self.tools['box'] = self.add_tool(
+                'meta.beta_diversity.distance_box')
+            self.on_rely(self.matrix, self.box_run)
+        if 'pcoa' in self.option('analysis'):
+            self.tools['pcoa'] = self.add_tool('meta.beta_diversity.pcoa')
+            self.on_rely(self.matrix, self.pcoa_run)
+        if 'nmds' in self.option('analysis'):
+            self.tools['nmds'] = self.add_tool('meta.beta_diversity.nmds')
+            self.on_rely(self.matrix, self.nmds_run)
+        if 'dbrda' in self.option('analysis'):
+            self.tools['dbrda'] = self.add_tool('meta.beta_diversity.dbrda')
+            self.on_rely(self.matrix, self.dbrda_run)
+        if 'hcluster' in self.option('analysis'):
+            self.tools['hcluster'] = self.add_tool(
+                'meta.beta_diversity.hcluster')
+            self.on_rely(self.matrix, self.hcluster_run)
+        if self.tools:
+            self.matrix_run()
+        if 'pca' in self.option('analysis'):
+            self.tools['pca'] = self.add_tool('meta.beta_diversity.pca')
+            self.pca_run()
+        if 'rda_cca' in self.option('analysis'):
+            self.tools['rda'] = self.add_tool('meta.beta_diversity.rda_cca')
+            self.rda_run()
+        self.on_rely(self.tools.values(), self.end)
