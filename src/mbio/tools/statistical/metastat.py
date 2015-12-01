1e630e48614a84795d9cd3e968d2ac9bb4197091
ping.qiu <ping.qiu@majorbio.com>
2015-12-01 11:33:57 +0800

    修改差异性模块

--- src/mbio/tools/statistical/metastat.py
+++ src/mbio/tools/statistical/metastat.py
@@ -20,41 +20,41 @@ class MetastatAgent(Agent):
             {"name": "chi_sample1", "type": "string"},  # 卡方检验的输入样品名称
             {"name": "chi_sample2", "type": "string"},  # 卡方检验的输入样品名称
             {"name": "chi_correction", "type": "string", "default": "none"},  # 卡方检验的多重检验校正
-            {"name": "chi_output", "type": "outfile", "format": "statistical.stat_table"},  # 卡方检验的输出结果
+            #{"name": "chi_output", "type": "outfile", "format": "statistical.stat_table"},  # 卡方检验的输出结果
             {"name": "fisher_input", "type": "infile", "format": "meta.otu.otu_table"},  # 费舍尔检验的输入文件
             {"name": "fisher_ci", "type": "float", "default": 0.05},  # 费舍尔检验的显著性水平
             {"name": "fisher_sample1", "type": "string"},  # 费舍尔检验的输入样品名称1
             {"name": "fisher_sample2", "type": "string"},  # 费舍尔检验的输入样品名称2
             {"name": "fisher_correction", "type": "string", "default": "none"},  # 费舍尔检验的多重检验校正
             {"name": "fisher_type", "type": "string", "default":"two.side"},  # 费舍尔检验的选择单尾或双尾检验
-            {"name": "fisher_output", "type": "outfile", "format": "statistical.stat_table"},  # 费舍尔检验的输出结果
+            #{"name": "fisher_output", "type": "outfile", "format": "statistical.stat_table"},  # 费舍尔检验的输出结果
             {"name": "kru_H_input", "type": "infile", "format": "meta.otu.otu_table"},  # kruskal_wallis_H_test的输入文件
             {"name": "kru_H_group", "type": "infile", "format": "meta.otu.group_table"},  # kruskal_wallis_H_test的输入分组文件
             {"name": "kru_H_correction", "type": "string", "default": "none"},  # kruskal_wallis_H_test的多重检验校正
-            {"name": "kru_H_output", "type": "outfile", "format": "statistical.stat_table"},  # kruskal_wallis_H_test的输出结果
+            #{"name": "kru_H_output", "type": "outfile", "format": "statistical.stat_table"},  # kruskal_wallis_H_test的输出结果
             {"name": "mann_input", "type": "infile", "format": "meta.otu.otu_table"},  # 秩和检验的输入文件
             {"name": "mann_ci", "type": "float", "default": 0.05},  # 秩和检验的显著性水平
             {"name": "mann_group", "type": "infile", "format": "meta.otu.group_table"},  # 秩和检验的输入分组文件
             {"name": "mann_correction", "type": "string", "default": "none"},  # 秩和检验的多重检验校正
             {"name": "mann_type", "type": "string", "default":"two.side"},  # 秩和检验的选择单尾或双尾检验
-            {"name": "mann_output", "type": "outfile", "format": "statistical.stat_table"},  # 秩和检验的输出结果
+            #{"name": "mann_output", "type": "outfile", "format": "statistical.stat_table"},  # 秩和检验的输出结果
             {"name": "student_input", "type": "infile", "format": "meta.otu.otu_table"},  # T检验的输入文件
             {"name": "student_ci", "type": "float", "default": 0.05},  # T检验的显著性水平
             {"name": "student_group", "type": "infile", "format": "meta.otu.group_table"},  # T检验的输入分组文件
             {"name": "student_correction", "type": "string", "default": "none"},  # T检验的多重检验校正
             {"name": "student_type", "type": "string", "default":"two.side"},  # T检验的选择单尾或双尾检验
-            {"name": "student_output", "type": "outfile", "format": "statistical.stat_table"},  # T检验的输出结果
+            #{"name": "student_output", "type": "outfile", "format": "statistical.stat_table"},  # T检验的输出结果
             {"name": "welch_input", "type": "infile", "format": "meta.otu.otu_table"},  # welch_T检验的输入文件
             {"name": "welch_ci", "type": "float", "default": 0.05},  # welch_T检验的显著性水平
             {"name": "welch_group", "type": "infile", "format": "meta.otu.group_table"},  # welch_T检验的输入分组文件
             {"name": "welch_correction", "type": "string", "default": "none"},  # welch_T检验的多重检验校正
             {"name": "welch_type", "type": "string", "default":"two.side"},  # welch_T检验的选择单尾或双尾检验
-            {"name": "welch_output", "type": "outfile", "format": "statistical.stat_table"},  # welch_T检验的输出结果
+            #{"name": "welch_output", "type": "outfile", "format": "statistical.stat_table"},  # welch_T检验的输出结果
             {"name": "anova_input", "type": "infile", "format": "meta.otu.otu_table"},  # anova分析的输入文件
             {"name": "anova_group", "type": "infile", "format": "meta.otu.group_table"},  # anova分析的输入分组文件
             {"name": "anova_correction", "type": "string", "default": "none"},  # anova分析的多重检验校正
-            {"name": "anova_output", "type": "outfile", "format": "statistical.stat_table"},  # anova分析的输出结果
-            {"name": "test", "type": "string"}
+            #{"name": "anova_output", "type": "outfile", "format": "statistical.stat_table"},  # anova分析的输出结果
+            {"name": "test", "type": "string"}   #选择统计学检验分析方法
         ]
         self.add_option(options)
 
@@ -201,30 +201,33 @@ class MetastatTool(Tool):
         将结果文件link到output文件夹下面
         :return:
         """
+        for root, dirs, files in os.walk(self.output_dir):
+            for names in files:
+                os.remove(os.path.join(root, names))
         files = os.listdir(self.work_dir)
         self.logger.info("设置结果目录")
         for f in files:
             if f == 'chi_result.xls':
                 os.link(self.work_dir + '/chi_result.xls', self.output_dir + '/chi_result.xls')
-                self.option('chi_output').set_path(self.output_dir + '/chi_result.xls')
+                #self.option('chi_output').set_path(self.output_dir + '/chi_result.xls')
             elif f == 'fisher_result.xls':
                 os.link(self.work_dir + '/fisher_result.xls', self.output_dir + '/fisher_result.xls')
-                self.option('fisher_output').set_path(self.output_dir + '/fisher_result.xls')
+                #self.option('fisher_output').set_path(self.output_dir + '/fisher_result.xls')
             elif f == 'student_result.xls':
                 os.link(self.work_dir + '/student_result.xls', self.output_dir + '/student_result.xls')
-                self.option('student_output').set_path(self.output_dir + '/student_result.xls')
+                #self.option('student_output').set_path(self.output_dir + '/student_result.xls')
             elif f == 'welch_result.xls':
                 os.link(self.work_dir + '/welch_result.xls', self.output_dir + '/welch_result.xls')
-                self.option('welch_output').set_path(self.output_dir + '/welch_result.xls')
+                #self.option('welch_output').set_path(self.output_dir + '/welch_result.xls')
             elif f == 'mann_result.xls':
                 os.link(self.work_dir + '/mann_result.xls', self.output_dir + '/mann_result.xls')
-                self.option('mann_output').set_path(self.output_dir + '/mann_result.xls')
+                #self.option('mann_output').set_path(self.output_dir + '/mann_result.xls')
             elif f == 'kru_H_result.xls':
                 os.link(self.work_dir + '/kru_H_result.xls', self.output_dir + '/kru_H_result.xls')
-                self.option('kru_H_output').set_path(self.output_dir + '/kru_H_result.xls')
+                #self.option('kru_H_output').set_path(self.output_dir + '/kru_H_result.xls')
             elif f == 'anova_result.xls':
                 os.link(self.work_dir + '/anova_result.xls', self.output_dir + '/anova_result.xls')
-                self.option('anova_output').set_path(self.output_dir + '/anova_result.xls')
+                #self.option('anova_output').set_path(self.output_dir + '/anova_result.xls')
         self.logger.info("设置结果目录成功")
 
 
