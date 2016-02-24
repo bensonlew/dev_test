f080c37a64eb05cab2a674093431fd014b0cad14
qindanhua <qdh504f@126.com>
2016-02-24 10:07:17 +0800

    多样性指数T检验相关修改

--- src/mbio/packages/alpha_diversity/group_file_split.py
+++ src/mbio/packages/alpha_diversity/group_file_split.py
@@ -21,21 +21,21 @@ def group_file_spilt(group_file, output_dir):
             # print line
             group_detail_list.append(line)
             group_detail[line[0]] = line[1]
-        group_name = {}.fromkeys(group_detail.values()).keys()
-        two_group_name = list(itertools.combinations(group_name, 2))
-        print(two_group_name)
-        n = 1
-        for two in two_group_name:
-            with open('two_group_file{}'.format(n), 'wb') as rf:
-                rf.write(first_line)
-                # print(two)
-                for detail in group_detail_list:
-                    # print(detail)
-                    for one in two:
-                        if detail[1] == one:
-                            line = '{}\t{}'.format(detail[0], one)
-                            print(line)
-                            rf.write('{}\n'.format(line))
-            shutil.move('two_group_file{}'.format(n), output_dir)
-            n += 1
+    group_name = {}.fromkeys(group_detail.values()).keys()
+    two_group_name = list(itertools.combinations(group_name, 2))
+    print(two_group_name)
+    n = 1
+    for two in two_group_name:
+        with open('two_group_file{}'.format(n), 'wb') as rf:
+            rf.write(first_line)
+            # print(two)
+            for detail in group_detail_list:
+                # print(detail)
+                for one in two:
+                    if detail[1] == one:
+                        line = '{}\t{}'.format(detail[0], one)
+                        print(line)
+                        rf.write('{}\n'.format(line))
+        shutil.move('two_group_file{}'.format(n), output_dir)
+        n += 1
     return output_dir
