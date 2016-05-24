2248ac7ef473846ae66891c30ccd7df3fcea0d45
shenghe <sheng.he@majorbio.com>
2016-05-24 08:35:26 +0800

    bug修复

--- src/mbio/workflows/meta/meta_base.py
+++ src/mbio/workflows/meta/meta_base.py
@@ -78,7 +78,7 @@ class MetaBaseWorkflow(Workflow):
             if not self.option("ref_fasta").is_set or not self.option("ref_taxon").is_set:
                 raise OptionError("数据库自定义模式必须设置ref_fasta和ref_taxon参数")
         else:
-            if self.option("database") not in ['silva119/16s_bacteria', 'silva119/16s_archaea',
+            if self.option("database") not in ['silva119/16s', 'silva119/16s_bacteria', 'silva119/16s_archaea',
                                                'silva119/18s_eukaryota', 'unite7.0/its_fungi', 'fgr/amoA', 'fgr/nosZ',
                                                'fgr/nirK', 'fgr/nirS', 'fgr/nifH', 'fgr/pmoA', 'fgr/mmoX']:
                 raise OptionError("数据库{}不被支持".format(self.option("database")))
