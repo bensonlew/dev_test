8532c797e0ca5f2747c542fdaf76f72d17c581b0
hesheng <sheng.he@majorbio.com>
2016-07-08 17:56:06 +0800

    beta即时计算bug修改

--- webroot/mainapp/controllers/project/meta_controller.py
+++ webroot/mainapp/controllers/project/meta_controller.py
@@ -5,6 +5,7 @@ from mainapp.libs.input_check import MetaCheck
 from mainapp.controllers.core.basic import Basic
 from mainapp.models.mongo.public.meta.meta import Meta
 from mainapp.libs.signature import check_sig
+from mainapp.models.mongo.distance_matrix import Distance
 
 
 class MetaController(Basic):
@@ -18,12 +19,17 @@ class MetaController(Basic):
         data = web.input()
         self.data = data
         self._client = data.client
-        otuId = data.otu_id
-        self._mainTableId = otuId
-        otu_info = Meta().get_otu_table_info(data.otu_id)
-        taskId = otu_info["task_id"]
+        if hasattr(data, 'otu_id'):
+            otuId = data.otu_id
+            self._mainTableId = otuId
+            table_info = Meta().get_otu_table_info(otuId)
+        else:
+            distance_id = data.specimen_distance_id
+            self._mainTableId = distance_id
+            table_info = Distance().get_distance_matrix_info(distance_id)
+        taskId = table_info["task_id"]
         self._taskId = taskId
-        self._projectSn = otu_info["project_sn"]
-        task_info = Meta().get_task_info(otu_info["task_id"])
+        self._projectSn = table_info["project_sn"]
+        task_info = Meta().get_task_info(table_info["task_id"])
         self._memberId = task_info["member_id"]
         super(MetaController, self).POST()
