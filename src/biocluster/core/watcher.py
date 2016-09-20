62dadd8b0bb54d717256f173570ec42cd9c55e68
郭权 <quan.guo@majorbio.com>
2016-09-20 17:01:42 +0800

    bug修复

--- src/biocluster/core/watcher.py
+++ src/biocluster/core/watcher.py
@@ -16,19 +16,22 @@ class Watcher(object):
     def add(self, func,  interval=1):
         """
         添加需要定时重运行的函数，并设置运行时间间隔
-        当函数返回值为False时，退出循环运行
+        当函数返回值为exit时，退出循环运行
 
         :param func:  需要执行的函数
         :param interval:  运行时间间隔
         :return: None
         """
         def watcher():
-
+            # print "add watcher %s " % func
             while True:
+                # print "%s is start" % func
                 # print "start watcher %s" % os.getpid()
+
+                if func() == "exit":
+                    # print "%s exit" % func
+                    return
                 gevent.sleep(interval)
-                if func() is False:
-                    break
                 # print "end watcher %s" % os.getpid()
         self._jobs.append(gevent.spawn(watcher))
 
