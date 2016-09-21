4bfa6b22b20fe51201a651cbce90bbcc153cdc13
qindanhua <qdh504f@126.com>
2016-09-21 12:50:20 +0800

    无参转录组api database 比对质量评估导表

--- src/mbio/api/database/denovo_rna_mapping.py
+++ src/mbio/api/database/denovo_rna_mapping.py
@@ -25,8 +25,8 @@ class DenovoRnaMapping(Base):
             for line in f:
                 line = line.strip().split()
                 data = {
-                    "project_sn": "pr_test",
-                    "task_id": "task_tse",
+                    "project_sn": self.bind_object.sheet.project_sn,
+                    "task_id": self.bind_object.sheet.project_sn,
                     "specimen_name": line[0],
                     "mapping_reads": line[1],
                     "mapping_rate": line[2]
@@ -58,8 +58,8 @@ class DenovoRnaMapping(Base):
     @report_check
     def add_rpkm_table(self, file_path, name=None, params=None):
         insert_data = {
-            "project_sn": "self.bind_object.sheet.project_sn",
-            "task_id": "self.bind_object.sheet.id",
+            "project_sn": self.bind_object.sheet.project_sn,
+            "task_id": self.bind_object.sheet.id,
             "name": name if name else "rpkm_origin",
             "status": "start",
             "params": json.dumps(params, sort_keys=True, separators=(',', ':')),
@@ -109,13 +109,7 @@ class DenovoRnaMapping(Base):
             sample_name = os.path.basename(rp).split(".")[0][6:]
             sam_dict = {}
             with open(rp, "r") as f:
-                # sampling_name = []
-                # n = 0
                 for line in f:
-                    # if re.match(r"name", line):
-                    #     sampling_time = -1
-                    #     n += 1
-                    #     sampling_name = line.strip().split("=c(")[1][:-1].split(",")
                     if re.match(r"S", line):
                         split_line = line.strip().split("=c(")
                         sampling_percent = split_line[0][1:]
@@ -184,3 +178,146 @@ class DenovoRnaMapping(Base):
             print("导入rpkm曲线数据出错:%s" % e)
         else:
             print("导入rpkm曲线数据成功")
+
+    @report_check
+    def add_coverage_table(self, coverage, name=None, params=None):
+        insert_data = {
+            "project_sn": self.bind_object.sheet.project_sn,
+            "task_id": self.bind_object.sheet.id,
+            "name": name if name else "coverage_origin",
+            "status": "start",
+            "desc": "",
+            "params": json.dumps(params, sort_keys=True, separators=(',', ':')),
+            "created_ts": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+        collection = self.db["sg_denovo_coverage"]
+        inserted_id = collection.insert_one(insert_data).inserted_id
+        self.add_coverage_detail(coverage, inserted_id)
+        return inserted_id
+
+    @report_check
+    def add_coverage_detail(self, coverage, coverage_id=None):
+        coverage_files = glob.glob("{}/*geneBodyCoverage.txt".format(coverage))
+        data_list = []
+        for cf in coverage_files:
+            sample_name = os.path.basename(cf).split(".")[0][9:]
+            # print sample_name
+            with open(cf, "r") as f:
+                percent = f.readline().strip().split()
+                value = f.next().strip().split()
+                plot_value = {}
+                for i in range(100):
+                    plot_value[percent[i+1]] = value[i+1]
+                # print plot_value
+                data = {
+                    "coverage_id": coverage_id,
+                    "specimen_name": sample_name,
+                    "plot_value": plot_value
+                }
+                data_list.append(data)
+        try:
+            collection = self.db["sg_denovo_coverage_detail"]
+            collection.insert_many(data_list)
+        except Exception, e:
+            print("导入rpkm曲线数据出错:%s" % e)
+        else:
+            print("导入rpkm曲线数据成功")
+
+    @report_check
+    def add_duplication_table(self, dup, name=None, params=None):
+        insert_data = {
+            "project_sn": self.bind_object.sheet.project_sn,
+            "task_id": self.bind_object.sheet.id,
+            "name": name if name else "duplication_origin",
+            "status": "start",
+            "desc": "",
+            "params": json.dumps(params, sort_keys=True, separators=(',', ':')),
+            "created_ts": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+        collection = self.db["sg_denovo_duplicate"]
+        inserted_id = collection.insert_one(insert_data).inserted_id
+        self.add_duplication_detail(dup, inserted_id)
+        return inserted_id
+
+    @report_check
+    def add_duplication_detail(self, dup, dup_id=None):
+        dup_seqs = glob.glob("{}/*seq.DupRate.xls".format(dup))
+        dup_poss = glob.glob("{}/*pos.DupRate.xls".format(dup))
+        data_list = []
+        for dp in dup_seqs:
+            sample_name = os.path.basename(dp).split(".")[0][4:]
+            with open(dp, "r") as f:
+                f.readline()
+                map_point = {}
+                for line in f:
+                    line = line.strip().split()
+                    map_point[line[0]] = line[1]
+                data = {
+                    "dup_id": dup_id,
+                    "specimen_name": sample_name,
+                    "map_point": map_point
+                }
+                data_list.append(data)
+
+        for ds in dup_poss:
+            sample_name = os.path.basename(ds).split(".")[0][4:]
+            with open(ds, "r") as f:
+                f.readline()
+                seq_point = {}
+                for line in f:
+                    line = line.strip().split()
+                    seq_point[line[0]] = line[1]
+            for dl in data_list:
+                if sample_name == dl["specimen_name"]:
+                    dl["seq_point"] = seq_point
+        try:
+            collection = self.db["sg_denovo_duplicate_detail"]
+            collection.insert_many(data_list)
+        except Exception, e:
+            print("导入冗余分析数据出错:%s" % e)
+        else:
+            print("导入冗余分析数据成功")
+
+    @report_check
+    def add_correlation_table(self, correlation, name=None, params=None):
+        correlation_tree = glob.glob("{}/*.tre".format(correlation))
+        with open(correlation_tree[0], "r") as t:
+            correlation_tree = t.readline().strip()
+        insert_data = {
+            "project_sn": self.bind_object.sheet.project_sn,
+            "task_id": self.bind_object.sheet.id,
+            "name": name if name else "correlation_origin",
+            "status": "start",
+            "desc": "",
+            "correlation_tree": correlation_tree,
+            "params": json.dumps(params, sort_keys=True, separators=(',', ':')),
+            "created_ts": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+        collection = self.db["sg_denovo_correlation"]
+        inserted_id = collection.insert_one(insert_data).inserted_id
+        self.add_correlation_detail(correlation, inserted_id)
+        return inserted_id
+
+    @report_check
+    def add_correlation_detail(self, collection, correlation_id=None):
+        correlation_matrix = collection + "/correlation_matrix.xls"
+        data_list = []
+        with open(correlation_matrix, "r") as m:
+            samples = m.readline().strip().split()
+            for line in m:
+                data = {
+                    "correlation_id": correlation_id
+                }
+                line = line.strip().split()
+                for i, s in enumerate(samples):
+                    data["specimen_name"] = line[0]
+                    data[s] = line[i+1]
+                # print data
+                data_list.append(data)
+        try:
+            collection = self.db["sg_denovo_correlation_detail"]
+            collection.insert_many(data_list)
+        except Exception, e:
+            print("导入相关系数分析数据出错:%s" % e)
+        else:
+            print("导入相关系数分析数据成功")
