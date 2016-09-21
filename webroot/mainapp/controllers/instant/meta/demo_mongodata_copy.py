8aa160f93e4dc2cebfa175ee75412a7edf94ba6d
hesheng <sheng.he@majorbio.com>
2016-09-21 14:28:05 +0800

    demo数据复制修改

--- webroot/mainapp/controllers/instant/meta/demo_mongodata_copy.py
+++ webroot/mainapp/controllers/instant/meta/demo_mongodata_copy.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-# __author__ = 'qiuping'
+# __author__ = 'hesheng'
 import web
 import json
 from mainapp.libs.signature import check_sig
+from biocluster.config import Config
 from mbio.packages.meta.copy_demo import CopyMongo
 
 
@@ -10,12 +11,18 @@ class DemoMongodataCopy(object):
     @check_sig
     def POST(self):
         data = web.input()
-        # client = data.client if hasattr(data, "client") else web.ctx.env.get('HTTP_CLIENT')
+        config = Config()
+        if config.MONGODB == 'tsanger':
+            db = 'tsanger'
+        elif config.MONGODB == 'sanger':
+            db = 'sanger'
+        else:
+            return json.dumps({"success": False, "info": "client不正确或者没有权限"})
         requires = ['task_id', 'target_task_id', 'target_project_sn', 'target_member_id']
         for i in requires:
             if not (hasattr(data, i)):
                 return json.dumps({"success": False, "info": "缺少%s参数!" % i})
-        copy_task = CopyMongo(data.task_id, data.target_task_id, data.target_project_sn, data.target_member_id)
+        copy_task = CopyMongo(data.task_id, data.target_task_id, data.target_project_sn, data.target_member_id, db=db)
         try:
             copy_task.run()
         except Exception as e:
