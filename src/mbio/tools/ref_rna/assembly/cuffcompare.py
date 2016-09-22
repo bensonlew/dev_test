2400f730c7a1b4705802bf22db39afdebf05eb51
wangzhaoyue <zhaoyue.wang@majorbio.local>
2016-09-22 14:24:12 +0800

    拼接部分的6的tool

---
+++ src/mbio/tools/ref_rna/assembly/cuffcompare.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+# __author__ = 'wangzhaoyue'
+from biocluster.agent import Agent
+from biocluster.tool import Tool
+from biocluster.core.exceptions import OptionError
+import os
+import shutil
+import re
+
+
+class CuffcompareAgent(Agent):
+    """
+    有参转录组cuffcompare合并
+    version v1.0.1
+    author: wangzhaoyue
+    last_modify: 2016.09.12
+    """
+    def __init__(self, parent):
+        super(CuffcompareAgent, self).__init__(parent)
+        options = [
+            {"name": "merged.gtf", "type": "infile","format":"ref_rna.assembly.gtf"},#拼接合并之后的转录本文件
+            {"name": "ref_fa", "type": "infile", "format": "sequence.fasta"},  # 参考基因文件
+            {"name": "ref_gtf", "type": "infile", "format": "ref_rna.assembly.gtf"},  # 参考基因的注释文件
+            {"name": "tmap", "type": "outfile", "format": "ref_rna.assembly.tmap"},  # compare后的tmap文件
+            {"name": "refmap", "type": "outfile", "format": "ref_rna.assembly.tmap"},  # compare后的refmap文件
+            {"name": "combined.gtf", "type": "outfile", "format": "ref_rna.assembly.gtf"},  # compare后的combined.gtf文件
+            {"name": "loci", "type": "outfile", "format": "ref_rna.assembly.loci"},  # compare后的loci文件
+            {"name": "stats", "type": "outfile", "format": "ref_rna.assembly.stats"},  # compare后的stats文件
+            {"name": "tracking", "type": "outfile", "format": "ref_rna.assembly.tracking"},  # compare后的tracking文件
+        ]
+        self.add_option(options)
+        self.step.add_steps("cuffcompare")
+        self.on('start', self.stepstart)
+        self.on('end', self.stepfinish)
+
+    def stepstart(self):
+        self.step.cuffcompare.start()
+        self.step.update()
+
+    def stepfinish(self):
+        self.step.cuffcompare.finish()
+        self.step.update()
+
+    def check_options(self):
+        """
+        重写参数检测函数
+        :return:
+        """
+        if not self.option('merged.gtf'):
+            raise OptionError('必须输入所有样本拼接合并后gtf文件')
+        if not self.option('ref_fa') :
+            raise OptionError('必须输入参考序列ref.fa')
+        if not self.option('ref_gtf'):
+            raise OptionError('必须输入参考序列ref.gtf')
+        return True
+
+    def set_resource(self):
+        """
+        设置所需资源，需在之类中重写此方法 self._cpu ,self._memory
+        :return:
+        """
+        self._cpu = 10
+        self._memory = "100G"
+
+    def end(self):
+        result_dir = self.add_upload_dir(self.output_dir)
+        result_dir.add_relpath_rules([
+            [".", "", "结果输出目录"],
+        ])
+        result_dir.add_regexp_rules([
+            # [".gtf", "gtf", ""]
+        ])
+        super(CuffcompareAgent, self).end()
+
+
+class CuffcompareTool(Tool):
+    def __init__(self, config):
+        super(CuffcompareTool, self).__init__(config)
+        self._version = "v1.0.1"
+        self.cuffcompare_path = 'bioinfo/rna/cufflinks-2.2.1/'
+        tmp = os.path.join(self.config.SOFTWARE_DIR, self.cuffcompare_path)
+        tmp = tmp + ":$PATH"
+        self.logger.debug(tmp)
+        self.set_environ(PATH=tmp)
+
+    def run(self):
+        """
+        运行
+        :return:
+        """
+        super(CuffcompareTool, self).run()
+        self.run_cuffcompare()
+        self.set_output()
+        self.end()
+
+    def run_cuffcompare(self):
+        """
+        运行cufflinks软件，进行拼接合并
+        """
+        cmd = self.cuffcompare_path +'cuffcompare -s {} -C -o {}cuffcmp -r {} {}'.format(self.option('ref_fa').prop['path'],self.work_dir+"/", self.option('ref_gtf').prop['path'] ,self.option('merged.gtf').prop['path'])
+        self.logger.info('运行cufflinks软件，进行比较')
+        command = self.add_command("cuffcompare_cmd", cmd).run()
+        self.wait(command)
+        if command.return_code == 0:
+            self.logger.info("cuffcompare运行完成")
+        else:
+            self.set_error("cuffcompare运行出错!")
+
+
+    def set_output(self):
+        """
+        将结果文件复制到output文件夹下面
+        :return:
+        """
+        self.logger.info("设置结果目录")
+        try:
+            shutil.copy2(self.work_dir + "/cuffcmp.combined.gtf",self.output_dir + "/cuffcmp.combined.gtf")
+            shutil.copy2(self.work_dir + "/cuffcmp.loci", self.output_dir + "/cuffcmp.loci")
+            shutil.copy2(self.work_dir + "/cuffcmp.stats", self.output_dir + "/cuffcmp.stats")
+            shutil.copy2(self.work_dir + "/cuffcmp.tracking", self.output_dir + "/cuffcmp.tracking")
+            self.option('combined.gtf').set_path(self.work_dir + "/cuffcmp.combined.gtf")
+            self.option('loci').set_path(self.work_dir + "/cuffcmp.loci")
+            self.option('stats').set_path(self.work_dir + "/cuffcmp.stats")
+            self.option('tracking').set_path(self.work_dir + "/cuffcmp.tracking")
+            self.logger.info("设置拼接比较结果目录成功")
+
+        except Exception as e:
+            self.logger.info("设置拼接比较分析结果目录失败{}".format(e))
+            self.set_error("设置拼接比较分析结果目录失败{}".format(e))
