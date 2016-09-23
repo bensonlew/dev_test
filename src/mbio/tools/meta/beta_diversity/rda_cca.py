ea830d73e90d1d18d72fe1839d1c6fa983de8baa
hesheng <sheng.he@majorbio.com>
2016-09-23 16:00:15 +0800

    beta_diversity tool内存设置

--- src/mbio/tools/meta/beta_diversity/rda_cca.py
+++ src/mbio/tools/meta/beta_diversity/rda_cca.py
@@ -89,7 +89,7 @@ class RdaCcaAgent(Agent):
         设置所需资源
         """
         self._cpu = 2
-        self._memory = ''
+        self._memory = '3G'
 
     def end(self):
         result_dir = self.add_upload_dir(self.output_dir)
