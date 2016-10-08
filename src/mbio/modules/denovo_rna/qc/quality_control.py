1cb650cda347d300e9cf0dcc116e3ce3762100a9
qindanhua <qdh504f@126.com>
2016-10-08 16:34:14 +0800

    修改质量剪切默认参数，长度与质量默认值

--- src/mbio/modules/denovo_rna/qc/quality_control.py
+++ src/mbio/modules/denovo_rna/qc/quality_control.py
@@ -30,8 +30,8 @@ class QualityControlModule(Module):
             {"name": "fq_l", "type": "outfile", "format": "sequence.fastq"},  # PE所有左端序列样本cat集合
             # {"name": "quality_a", "type": "int", "default": 30},  # 去接头碱基质量
             # {"name": "length_a", "type": "int", "default": 30},  # 去接头碱基长度
-            {"name": "quality_q", "type": "int", "default": 20},  # 质量剪切碱基质量
-            {"name": "length_q", "type": "int", "default": 30}  # 质量剪切碱基长度
+            {"name": "quality_q", "type": "int", "default": 30},  # 质量剪切碱基质量
+            {"name": "length_q", "type": "int", "default": 50}  # 质量剪切碱基长度
         ]
         self.add_option(options)
         self.samples = {}
