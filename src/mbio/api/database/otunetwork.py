e568110b68a0b75d064b5025f957eef0b74ce658
xuanhongdong <hongdong.xuan@majorbio.com>
2016-12-02 15:14:56 +0800

    mongo 导表api

--- src/mbio/api/database/otunetwork.py
+++ src/mbio/api/database/otunetwork.py
@@ -33,12 +33,12 @@ class Otunetwork(Base):
                 line = line.strip('\n')
                 line_data = line.split('\t')
                 data_list1.append(line_data)
-            data = [("network_id", table_id), ("Transitivity", eval(data_list1[0][1])),
-                    ("Diameter", eval(data_list1[1][1])), ("Average_shortest_path_length", eval(data_list1[2][1]))]
+            data = [("network_id", table_id), ("transitivity", eval(data_list1[0][1])),
+                    ("diameter", eval(data_list1[1][1])), ("average_shortest_path_length", eval(data_list1[2][1]))]
             data_son = SON(data)
             data_list.append(data_son)
         try:
-            collection = self.db["sg_meta_network_attributes"]
+            collection = self.db["sg_network_structure_attributes"]
             collection.insert_many(data_list)
         except Exception, e:
             self.bind_object.logger.error("导入%s信息出错:%s" % (file_path, e))
@@ -47,7 +47,7 @@ class Otunetwork(Base):
         return data_list, table_id
 
     # @report_check
-    def add_network_degree(self, file_path, table_id = None, group_id = None, from_otu_table = None, level_id = None, major = False):
+    def add_network_degree(self, file1_path, file2_path, file3_path, table_id = None, group_id = None, from_otu_table = None, level_id = None, major = False):
         if major:
             table_id = self.create_otunetwork(self, params, group_id, from_otu_table, level_id)
         else:
@@ -59,25 +59,32 @@ class Otunetwork(Base):
             else:
                 raise Exception("table_id必须为ObjectId对象或其对应的字符串!")
         data_list = []
-        with open(file_path, 'rb') as r:
-            i = 0
-            for line in r:
-                if i == 0:
-                    i = 1
-                else:
-                    line = line.strip('\n')
-                    line_data = line.split('\t')
-                    data = [("network_id", table_id), ("OTU_Degree", line_data[0]), ("OTU_Num", line_data[1]), ("Sample_Degree", line_data[2]),
-                            ("Sample_Num", line_data[3]), ("Node_Degree", line_data[4]), ("Node_Num", line_data[5])]
-                    data_son = SON(data)
-                    data_list.append(data_son)
+        with open(file1_path, 'rb') as r, open(file2_path, 'rb') as h, open(file3_path, 'rb') as w:
+            data1 = r.readlines()[2:]
+            data2 = h.readlines()[2:]
+            data3 = w.readlines()[2:]
+            for line1 in data1:
+                line1 = line1.strip().split("\t")
+                data1 = [("network_id", table_id), ("degree", line1[0]), ("num", line1[1]), ("type", "OTU")]
+                data_son1 = SON(data1)
+                data_list.append(data_son1)
+            for line2 in data2:
+                line2 = line2.strip().split("\t")
+                data2 = [("network_id", table_id), ("degree", line2[0]), ("num", line2[1]), ("type", "sample")]
+                data_son2 = SON(data2)
+                data_list.append(data_son2)
+            for line3 in data3:
+                line3 = line3.strip().split("\t")
+                data3 = [("network_id", table_id), ("degree", line3[0]), ("num", line3[1]), ("type", "node")]
+                data_son3 = SON(data3)
+                data_list.append(data_son3)
         try:
-            collection = self.db["sg_meta_network_degree"]
+            collection = self.db["sg_network_distribution_node"]
             collection.insert_many(data_list)
         except Exception, e:
-            self.bind_object.logger.error("导入%s信息出错:%s" % (file_path, e))
+            self.bind_object.logger.error("导入%s信息出错:%s" % (file3_path, e))
         else:
-            self.bind_object.logger.info("导入%s信息成功!" % file_path)
+            self.bind_object.logger.info("导入%s信息成功!" % file3_path)
         return data_list
 
     # @report_check
@@ -101,13 +108,22 @@ class Otunetwork(Base):
                 else:
                     line = line.strip('\n')
                     line_data = line.split('\t')
-                    data = [("network_id", table_id), ("Node_ID", eval(line_data[0])),
-                            ("Node_Name", line_data[1]),("Degree_Centrality", eval(line_data[2])),
-                            ("Closeness_Centrality", eval(line_data[3])), ("Betweenness_Centrality", eval(line_data[4]))]
+                    match_obj = re.match('d__', line_data[1])
+                    if match_obj is not None:
+                        data = [("network_id", table_id), ("node_id", eval(line_data[0])),
+                            ("node_name", line_data[1]),("node_disp_name", line_data[1].strip().split(";")[-1:][0]),("degree_centrality", eval(line_data[2])),
+                            ("closeness_centrality", eval(line_data[3])), ("betweenness_centrality", eval(line_data[4]))]
+                    else:
+                        data = [("network_id", table_id), ("node_id", eval(line_data[0])),
+                                ("node_name", line_data[1]),
+                                ("node_disp_name", line_data[1]),
+                                ("degree_centrality", eval(line_data[2])),
+                                ("closeness_centrality", eval(line_data[3])),
+                                ("betweenness_centrality", eval(line_data[4]))]
                     data_son = SON(data)
                     data_list.append(data_son)
         try:
-            collection = self.db["sg_meta_network_centrality"]
+            collection = self.db["sg_network_centrality_node"]
             collection.insert_many(data_list)
         except Exception, e:
             self.bind_object.logger.error("导入%s信息出错:%s" % (file_path, e))
@@ -136,14 +152,14 @@ class Otunetwork(Base):
                 if match_obj is not None:
                     line1 = line_data[0].strip().split(';')
                     data = [("network_id", table_id), ("node_name", line_data[0]), ("node_disp_name", line1[len(line1)-1]),
-                            ("ntype", line_data[1]), ("degree", eval(line_data[2])), ("weighted_degree", " ")]
+                            ("ntype", "species_node"), ("degree", eval(line_data[3])), ("weighted_degree", float(line_data[4]))]
                 else:
                     data = [("network_id", table_id), ("node_name", line_data[0]), ("node_disp_name", line_data[1]),
-                            ("ntype", line_data[2]), ("degree", eval(line_data[3])), ("weighted_degree", float(line_data[4]))]
+                            ("ntype", "sample_node"), ("degree", eval(line_data[3])), ("weighted_degree", float(line_data[4]))]
                 data_son = SON(data)
                 data_list.append(data_son)
         try:
-            collection = self.db["sg_meta_network_node_table"]
+            collection = self.db["sg_network_structure_node"]
             collection.insert_many(data_list)
         except Exception, e:
             self.bind_object.logger.error("导入%s信息出错:%s" % (file_path, e))
@@ -152,46 +168,46 @@ class Otunetwork(Base):
         return data_list
 
     # @report_check
-    def add_node_table_group(self, file_path, params=None, group_id=None, from_otu_table=None, table_id=None, major=False):
-        #该步是用于添加后面画网络图的时候，节点进行分组显示，传入的数据是workflow生成文件是real_node_table.txt。
-        if major:
-            table_id = self.create_otunetwork(self, params, group_id, from_otu_table, level_id)
-        else:
-            if table_id is None:
-                raise Exception("major为False时需提供table_id!")
-            if not isinstance(table_id, ObjectId):
-                if isinstance(table_id, StringTypes):
-                    table_id = ObjectId(table_id)
-                else:
-                    raise Exception("table_id必须为ObjectId对象或其对应的字符串!")
-        data_list = []
-        with open(file_path, 'rb') as r:
-            data_line = r.readlines()[1:]
-            for line in data_line:
-                line_data = line.strip().split('\t')
-                match_obj = re.match('d__', line_data[0])
-                if match_obj is not None:
-                    line1 = line_data[0].strip().split(';')
-                    line2 = line1[len(line1)-1]
-                    data = [("network_id", table_id), ("id", line2),
-                            ("group", line2[0:3])]
-                else:
-                    line1 = line_data[0].strip().split("_")
-                    l = len(line1)
-                    line2 = line1[0:l-1]
-                    line3 = "_".join(line2)
-                    data = [("network_id", table_id), ("id", line_data[0]),
-                            ("group", line3)]
-                data_son = SON(data)
-                data_list.append(data_son)
-        try:
-            collection = self.db["sg_meta_network_node_table_group"]
-            collection.insert_many(data_list)
-        except Exception, e:
-            self.bind_object.logger.error("导入%s信息出错:%s" % (file_path, e))
-        else:
-            self.bind_object.logger.info("导入%s信息成功!" % file_path)
-        return data_list
+    # def add_node_table_group(self, file_path, params=None, group_id=None, from_otu_table=None, table_id=None, major=False):
+    #     #该步是用于添加后面画网络图的时候，节点进行分组显示，传入的数据是workflow生成文件是real_node_table.txt。
+    #     if major:
+    #         table_id = self.create_otunetwork(self, params, group_id, from_otu_table, level_id)
+    #     else:
+    #         if table_id is None:
+    #             raise Exception("major为False时需提供table_id!")
+    #         if not isinstance(table_id, ObjectId):
+    #             if isinstance(table_id, StringTypes):
+    #                 table_id = ObjectId(table_id)
+    #             else:
+    #                 raise Exception("table_id必须为ObjectId对象或其对应的字符串!")
+    #     data_list = []
+    #     with open(file_path, 'rb') as r:
+    #         data_line = r.readlines()[1:]
+    #         for line in data_line:
+    #             line_data = line.strip().split('\t')
+    #             match_obj = re.match('d__', line_data[0])
+    #             if match_obj is not None:
+    #                 line1 = line_data[0].strip().split(';')
+    #                 line2 = line1[len(line1)-1]
+    #                 data = [("network_id", table_id), ("id", line2),
+    #                         ("group", line2[0:3])]
+    #             else:
+    #                 line1 = line_data[0].strip().split("_")
+    #                 l = len(line1)
+    #                 line2 = line1[0:l-1]
+    #                 line3 = "_".join(line2)
+    #                 data = [("network_id", table_id), ("id", line_data[0]),
+    #                         ("group", line3)]
+    #             data_son = SON(data)
+    #             data_list.append(data_son)
+    #     try:
+    #         collection = self.db["sg_meta_network_node_table_group"]
+    #         collection.insert_many(data_list)
+    #     except Exception, e:
+    #         self.bind_object.logger.error("导入%s信息出错:%s" % (file_path, e))
+    #     else:
+    #         self.bind_object.logger.info("导入%s信息成功!" % file_path)
+    #     return data_list
 
     #@report_check
     def add_edge_table(self, file_path, table_id=None, group_id=None, from_otu_table=None, level_id=None,
@@ -217,7 +233,7 @@ class Otunetwork(Base):
                 data_son = SON(data)
                 data_list.append(data_son)
         try:
-            collection = self.db["sg_meta_network_link"]
+            collection = self.db["sg_network_structure_link"]
             collection.insert_many(data_list)
         except Exception, e:
             self.bind_object.logger.error("导入%s信息出错:%s" % (file_path, e))
@@ -257,7 +273,7 @@ class Otunetwork(Base):
             "status": "end",
             "created_ts": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         }
-        collection = self.db["sg_meta_otunetwork"]
+        collection = self.db["sg_network"]
         inserted_id = collection.insert_one(insert_data).inserted_id
         return inserted_id
 
