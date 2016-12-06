8b55685ab2646536fe7e0ff3926a7dc5ff6f9c57
hesheng <sheng.he@majorbio.com>
2016-12-06 14:12:42 +0800

    树图导表修改

--- src/mbio/api/database/phylo_tree.py
+++ src/mbio/api/database/phylo_tree.py
@@ -33,38 +33,38 @@ class PhyloTree(Base):
                      ('newicktree', open(self.bind_object.output_dir + '/phylo_tree.tre').read())]
         self.main_id = collection.insert_one(SON(main_data)).inserted_id
         if self.bind_object.sheet.option('color_level_id'):
-            self._add_species_categries()
-        self.specimen_categries = self._add_format_otu()
-        collection.update_one({'_id': self.main_id}, {'$set': {'specimen_categries': self.specimen_categries}})
+            self._add_species_categories()
+        self.specimen_categories = self._add_format_otu()
+        collection.update_one({'_id': self.main_id}, {'$set': {'specimen_categories': self.specimen_categories}})
         self.bind_object.logger.info('Phylo tree导入数据库完成。')
         return str(self.main_id)
 
-    def _add_species_categries(self):
+    def _add_species_categories(self):
         species_group = self.bind_object.work_dir + '/species_group.xls'
         if not os.path.isfile(species_group):
-            self.categries = None
-            return self.categries
-        collection = self.db['sg_phylo_tree_species_catergries']
+            self.categories = None
+            return self.categories
+        collection = self.db['sg_phylo_tree_species_categories']
         with open(species_group) as f:
             f.readline()
             group = defaultdict(list)
             for i in f:
                 line_sp = i.strip().split('\t')
                 group[line_sp[1]].append(line_sp[0])
-            insert_data = [('phylo_tree_id', self.main_id), ('categries', group.keys()), ('species', group.values())]
+            insert_data = [('phylo_tree_id', self.main_id), ('categories', group.keys()), ('species', group.values())]
             collection.insert_one(SON(insert_data))
-            self.categries = group.keys()
-            return self.categries
+            self.categories = group.keys()
+            return self.categories
 
     def _add_format_otu(self):
         collection = self.db['sg_phylo_tree_species_detail']
         with open(self.bind_object.output_dir + '/species_table.xls') as f:
-            categries = f.readline().rstrip().split('\t')[1:]
-            categries.insert(0, 'species_name')
+            categories = f.readline().rstrip().split('\t')[1:]
+            categories.insert(0, 'species_name')
             insert_data = []
             for i in f:
-                one = zip(categries, i.strip().split('\t'))
+                one = zip(categories, i.strip().split('\t'))
                 one.insert(0, ('phylo_tree_id', self.main_id))
                 insert_data.append(SON(one))
         collection.insert_many(insert_data)
-        return categries[1:]
+        return categories[1:]
