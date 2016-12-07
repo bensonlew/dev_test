f6f9e5a3061c56ef608dfe84f35184514e69dfb4
hesheng <sheng.he@majorbio.com>
2016-12-07 16:19:42 +0800

    说明修改

--- webroot/mainapp/controllers/report/download_web_pic.py
+++ webroot/mainapp/controllers/report/download_web_pic.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 # __author__ = 'shenghe'
-# from mainapp.libs.signature import check_sig
 from gevent import monkey; monkey.patch_all()
+from mainapp.libs.signature import check_sig
 import web
 import json
 import tempfile
+import HTMLParser
 from gevent.subprocess import Popen
 
 
 class DownloadWebPic(object):
-    # @check_sig
-    def POST(self, my_type):
+    """
+    网页的svg转化成pdf或者png等图片格式
+    POST参数有(均为必须参数) "file_type", "file_name", "svg_data", "scale"
+    分别为 文件类型(必须为png或者pdf), 文件名称(暂时没有用处，但是会放在header中),svg数据,放大大小(在png模式下生效)
+    """
+
+    @check_sig
+    def POST(self, my_type=None):
         data = web.input()
         print('Convert Type: {}'.format(my_type))
         for i in ["file_type", "file_name", "svg_data", "scale"]:
@@ -33,14 +40,19 @@ class DownloadWebPic(object):
             web.header('Content-disposition', 'attachment; filename={}'.format(data.file_name))
             return open(file_pic, 'rb').read()
         else:
-            msg = {"success": False, "info": "生成结果文件出错".format(data.scale)}
+            msg = {"success": False, "info": "生成图片文件出错".format(data.scale)}
             return json.dumps(msg)
 
     def _svg_convert(self):
+        """
+        转换svg为图片，使用python3的包cairosvg命令行形式生成
+        """
         temp_dir = tempfile.mkdtemp()
         temp_svg = temp_dir + '/temp.svg'
         with open(temp_svg, 'wb') as w:
-            w.write(web.input().svg_data)
+            parser = HTMLParser.HTMLParser()
+            svg_data = parser.unescape(web.input().svg_data)  # html转义
+            w.write(svg_data)
         temp_pic = temp_dir + '/temp.' + web.input().file_type
         cmd = 'cairosvg {} -f {} -o {} -s {}'.format(temp_svg, web.input().file_type, temp_pic, int(web.input().scale))
         pro = Popen(cmd, shell=True)
@@ -54,8 +66,4 @@ class DownloadWebPic(object):
 
 
 if __name__ == '__main__':
-    print tempfile.mkdtemp()
-    a = Popen("dir ./fsa", shell=True)
-    a.wait()
-    print a.returncode
-    print 'hhhhhhhhhhhhhhhhhhhh'
+    pass
