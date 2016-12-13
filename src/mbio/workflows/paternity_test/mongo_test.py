13b3d157fc1de29b7998bacb57428ceb2c13ba8e
moli.zhou <moli.zhou@majorbio.com>
2016-12-13 09:02:53 +0800

    add pt_analysis and save the results into MONGO

--- src/mbio/workflows/paternity_test/mongo_test.py
+++ src/mbio/workflows/paternity_test/mongo_test.py
@@ -21,18 +21,32 @@ class PtProcessWorkflow(Workflow):
 			{"name": "fastq_path", "type": "string"},  # fastq所在路径
 			{"name": "cpu_number", "type": "int", "default": 4},  # cpu个数
 			{"name": "ref_fasta", "type": "infile", "format": "sequence.fasta"},  # 参考序列
-			{"name": "targets_bedfile", "type": "string"}
+			{"name": "targets_bedfile", "type": "string"},
+
+			{"name": "dad_id", "type": "string"},  # 输入F/M/S的样本ID
+			{"name": "mom_id", "type": "string"},
+			{"name": "preg_id", "type": "string"},
+			{"name": "err_min", "type": "int", "default": 2},  # 允许错配数
+			{"name": "ref_point", "type": "string"},  # 参考位点
 		]
 		self.add_option(options)
-
+		self.pt_analysis = self.add_module("paternity_test.pt_analysis")
 		self.tools = []
 		self.set_options(self._sheet.options())
+		self.step.add_steps("pt_analysis", "result", "retab",
+		                    "de_dup1", "de_dup2")
 
 	def check_options(self):
 		'''
 		检查参数设置
 		'''
 
+		if not self.option("dad_id"):
+			raise OptionError("必须输入父本编号")
+		if not self.option("mom_id"):
+			raise OptionError("必须输入母本编号")
+		if not self.option("preg_id"):
+			raise OptionError("必须输入胎儿编号")
 		if not self.option("ref_fasta").is_set:
 			raise OptionError("必须输入参考基因组的fastq文件")
 		if not self.option('fastq_path'):
@@ -86,6 +100,21 @@ class PtProcessWorkflow(Workflow):
 		for tool in self.tools:
 			tool.run()
 
+
+	def pt_analysis_run(self):
+		api_read_tab = self.api.tab_file
+		self.pt_analysis.set_options({
+			"dad_tab": api_read_tab.export_tab_file(self.option('dad_id'), self.output_dir),  # 数据库的tab文件
+			"mom_tab": api_read_tab.export_tab_file(self.option('mom_id'), self.output_dir),
+			"preg_tab": api_read_tab.export_tab_file(self.option('preg_id'), self.output_dir),
+			"ref_point": self.option("ref_point"),
+			"err_min": self.option("err_min")
+		})
+		self.pt_analysis.on('end', self.set_output, 'pt_analysis')
+		self.pt_analysis.on('start', self.set_step, {'start': self.step.pt_analysis})
+		self.pt_analysis.on('end', self.set_step, {'end': self.step.pt_analysis})
+		self.pt_analysis.run()
+
 	def linkdir(self, dirpath, dirname):
 		"""
 		link一个文件夹下的所有文件到本module的output目录
@@ -116,15 +145,32 @@ class PtProcessWorkflow(Workflow):
 		if event['data'] == "fastq2tab":
 			self.linkdir(obj.output_dir + '/bam2tab', self.output_dir)
 			api = self.api.tab_file
-			temp = os.listdir(self.output_dir)
+			temp = os.listdir(obj.output_dir + '/bam2tab')
+			api_read_tab = self.api.tab_file  # 二次判断数据库中是否存在tab文件
 			for i in temp:
-				if re.search(r'.*.tab$', i):
+				m = re.search(r'(.*)\.mem.*tab$', i)
+				if m:
 					tab_path = self.output_dir +'/' + i
-			api.add_sg_pt_tab_detail(tab_path)
+					tab_name = m.group(1)
+					if not api_read_tab.tab_exist(tab_name):
+						api.add_sg_pt_tab_detail(tab_path)
+
+		if event['data'] == "pt_analysis":
+			self.linkdir(obj.output_dir +'/family_analysis', self.output_dir)
+			self.linkdir(obj.output_dir + '/family_merge', self.output_dir)
+			api_pt = self.api.sg_paternity_test
+			file = self.output_dir + '/family_analysis.txt'
+			api_pt.add_sg_pt_family_detail(file)
 
 	def run(self):
+		if self.tools:
+			self.pt_analysis.on('end', self.end)
+			self.on_rely(self.tools, self.pt_analysis_run)
+		else:
+			self.pt_analysis.on('end', self.end)
+			self.pt_analysis_run()
 		self.fastq2tab_run()
-		self.on_rely(self.tools,self.end)
+
 		super(PtProcessWorkflow,self).run()
 
 	def end(self):
