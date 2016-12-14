1290d0321959c99a15d4d5ec0123a284b7d1807c
zengjing <jing.zeng@majorbio.local>
2016-12-14 16:10:19 +0800

    function_predict.py

--- webroot/mainapp/controllers/submit/meta/function_predict.py
+++ webroot/mainapp/controllers/submit/meta/function_predict.py
@@ -5,6 +5,7 @@ import web
 import json
 import datetime
 import random
+from mainapp.libs.param_pack import group_detail_sort
 from mainapp.libs.signature import check_sig
 from mainapp.models.workflow import Workflow
 from mainapp.models.mongo.meta import Meta
@@ -18,16 +19,21 @@ class FunctionPredict(object):
     def POST(self):
         data = web.input()
         client = data.client if hasattr(data, "client") else web.ctx.env.get('HTTP_CLIENT')
-        params_name = ['otu_id', 'submit_location']
+        params_name = ['otu_id', 'submit_location', "group_id", "group_detail"]
         success = []
         print data
         for param in params_name:
             if not hasattr(data, param):
                 info = {"success": False, "info": "缺少{}参数".format(param)}
                 return json.dumps(info)
+        group_detail = json.loads(data.group_detail)
+        if not isinstance(group_detail, dict):
+            success.append("传入的group_detail不是一个字典")
         my_param = dict()
         my_param['otu_id'] = data.otu_id
         my_param['submit_location'] = data.submit_location
+        my_param['group_id'] = data.group_id
+        my_param['group_detail'] = group_detail_sort(data.group_detail)
         params = json.dumps(my_param, sort_keys=True, separators=(',', ':'))
         otu_info = Meta().get_otu_table_info(data.otu_id)
         if otu_info:
@@ -40,7 +46,7 @@ class FunctionPredict(object):
                 info = {"success": False, "info": "这个otu表对应的task:{}没有member_id!".format(otu_info["task_id"])}
                 return json.dumps(info)
             predict_id = FP().add_function_predict(name=None, params=params, otu_id=data.otu_id)
-            update_info = {str(predict_id): "sg_16s_function_predict"}
+            update_info = {str(predict_id): "sg_16s_function_prediction"}
             update_info = json.dumps(update_info)
             (output_dir, update_api) = GetUploadInfo(client, member_id, project_sn, task_id, '16s_function_predict')
             workflow_id = self.get_new_id(task_id, data.otu_id)
@@ -51,7 +57,7 @@ class FunctionPredict(object):
                 "type": "workflow",
                 "client": client,
                 "project_sn": project_sn,
-                "to_file": ["function_predict.export_otu_table(otu_table)", "function_predict.export_otu_rep(otu_reps)"],
+                "to_file": ["function_predict.export_otu_table_by_detail(otu_table)"],
                 "USE_DB": True,
                 "IMPORT_REPORT_DATA": True,
                 "UPDATE_STATUS_API": update_api,
@@ -60,7 +66,7 @@ class FunctionPredict(object):
                 "options": {
                     "update_info": update_info,
                     "otu_table": data.otu_id,
-                    "otu_reps": data.otu_id,
+                    "group_detail": data.group_detail,
                     "predict_id": str(predict_id)
                 }
             }
