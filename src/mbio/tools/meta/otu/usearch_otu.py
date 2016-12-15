691fc43954bd4633db747f367a961a5418cfb789
hesheng <sheng.he@majorbio.com>
2016-12-15 14:00:26 +0800

    修改Usearch OTU的错误重运行，修改command

--- src/mbio/tools/meta/otu/usearch_otu.py
+++ src/mbio/tools/meta/otu/usearch_otu.py
@@ -22,12 +22,18 @@ class UsearchOtuAgent(Agent):
     def __init__(self, parent=None):
         super(UsearchOtuAgent, self).__init__(parent)
         options = [
-            {'name': 'fasta', 'type': 'infile', 'format': 'sequence.fasta'},  # 输入fasta文件，序列名称格式为'>sampleID_seqID'.
-            {'name': 'identity', 'type': 'float', 'default': 0.97},  # 相似性值，范围0-1.
-            {'name': 'otu_table', 'type': 'outfile', 'format': 'meta.otu.otu_table'},  # 输出结果otu表
-            {'name': 'otu_rep', 'type': 'outfile', 'format': 'sequence.fasta'},  # 输出结果otu代表序列
-            {'name': 'otu_seqids', 'type': 'outfile', 'format': 'meta.otu.otu_seqids'},  # 输出结果otu中包含序列列表
-            {'name': 'otu_biom', 'type': 'outfile', 'format': 'meta.otu.biom'}  # 输出结果biom格式otu表
+            # 输入fasta文件，序列名称格式为'>sampleID_seqID'.
+            {'name': 'fasta', 'type': 'infile', 'format': 'sequence.fasta'},
+            # 相似性值，范围0-1.
+            {'name': 'identity', 'type': 'float', 'default': 0.97},
+            {'name': 'otu_table', 'type': 'outfile',
+                'format': 'meta.otu.otu_table'},  # 输出结果otu表
+            {'name': 'otu_rep', 'type': 'outfile',
+                'format': 'sequence.fasta'},  # 输出结果otu代表序列
+            {'name': 'otu_seqids', 'type': 'outfile',
+                'format': 'meta.otu.otu_seqids'},  # 输出结果otu中包含序列列表
+            {'name': 'otu_biom', 'type': 'outfile',
+                'format': 'meta.otu.biom'}  # 输出结果biom格式otu表
         ]
         self.add_option(options)
         self.step.add_steps('OTUCluster')
@@ -89,37 +95,45 @@ class UsearchOtuTool(Tool):
         self.qiime_path = "program/Python/bin/"
 
     def cmd1(self):
-        cmd = self.usearch_path+"uparse -derep_prefix meta.fasta -output meta_derepprefix.fasta -sizeout"
+        cmd = self.usearch_path + \
+            "uparse -derep_prefix meta.fasta -output meta_derepprefix.fasta -sizeout"
         return cmd
 
     def cmd2(self):
-        cmd = self.usearch_path+"uparse -sortbysize meta_derepprefix.fasta -output meta_derepprefix_sorted.fasta -minsize 2"
+        cmd = self.usearch_path + \
+            "uparse -sortbysize meta_derepprefix.fasta -output meta_derepprefix_sorted.fasta -minsize 2"
         return cmd
 
     def cmd3(self):
-        ratio = str(100-float(self.option('identity'))*100)
-        cmd = self.usearch_path+"uparse -cluster_otus meta_derepprefix_sorted.fasta -otus cluster.fasta -otu_radius_pct "+ratio
+        ratio = str(100 - float(self.option('identity')) * 100)
+        cmd = self.usearch_path + \
+            "uparse -cluster_otus meta_derepprefix_sorted.fasta -otus cluster.fasta -otu_radius_pct " + ratio
         return cmd
 
     def cmd4(self):
-        cmd = self.usearch_path+"uparse -usearch_global meta.fasta -db cluster.fasta -strand plus -id "+str(self.option('identity'))+" -uc map.uc"
+        cmd = self.usearch_path + "uparse -usearch_global meta.fasta -db cluster.fasta -strand plus -id " + \
+            str(self.option('identity')) + " -uc map.uc"
         return cmd
 
     def cmd5(self):
-        cmd = self.script_path+"""uc2otuseqids.pl -i map.uc -o cluster.seqids"""
+        cmd = self.script_path + """uc2otuseqids.pl -i map.uc -o cluster.seqids"""
         return cmd
 
     def cmd6(self):
         os.system("""cat cluster.seqids|awk '{split($0,line,\"\\t\");new=line[1];for(i=2;i<NF+1;i++){match(line[i],/_[^_]+$/);smp=substr(line[i],1,RSTART-1);id=substr(line[i],RSTART+1,RLENGTH);nsmp=smp;gsub(/_/,\".\",nsmp);new=new\"\\t\"nsmp\"_\"id;print nsmp\"\\t\"smp;print line[i]\"\\t\"smp >\"cluster.groups\"};print new >\"cluster.seqids.tmp\";}'|sort|uniq >name.check""")
-        os.system("""awk '{ print $1,\"OTU\"NR >\"cluster2otu.rename\";$1=\"OTU\"NR;print $0 }' cluster.seqids|sed 's/ /\\t/g' >otu_seqids.txt""")
-        os.system("""awk '{$1=\"OTU\"NR;print $0}' cluster.seqids.tmp|sed 's/ /\\t/g' > otu.seqids.tmp""")
-        cmd = self.qiime_path+"""make_otu_table.py -i otu.seqids.tmp  -o otu_table.biom"""
+        os.system(
+            """awk '{ print $1,\"OTU\"NR >\"cluster2otu.rename\";$1=\"OTU\"NR;print $0 }' cluster.seqids|sed 's/ /\\t/g' >otu_seqids.txt""")
+        os.system(
+            """awk '{$1=\"OTU\"NR;print $0}' cluster.seqids.tmp|sed 's/ /\\t/g' > otu.seqids.tmp""")
+        cmd = self.qiime_path + """make_otu_table.py -i otu.seqids.tmp  -o otu_table.biom"""
         return cmd
 
     def cmd7(self):
-        os.system("""cat name.check|awk '{gsub(/\\./,\"\\\\.\",$1);print \"sed '\\''s/\\\"\"$1\"\\\"/\\\"\"$2\"\\\"/g'\\''  otu_table.biom >otu_table.biom.tmp\\nmv otu_table.biom.tmp otu_table.biom\";}' >otu.name.check.sh""")
+        os.system(
+            """cat name.check|awk '{gsub(/\\./,\"\\\\.\",$1);print \"sed '\\''s/\\\"\"$1\"\\\"/\\\"\"$2\"\\\"/g'\\''  otu_table.biom >otu_table.biom.tmp\\nmv otu_table.biom.tmp otu_table.biom\";}' >otu.name.check.sh""")
         os.system("""sh otu.name.check.sh""")
-        cmd = self.qiime_path+"""biom convert -i otu_table.biom -o otu_table.txt  --table-type \"OTU table\"  --to-tsv"""
+        cmd = self.qiime_path + \
+            """biom convert -i otu_table.biom -o otu_table.txt  --table-type \"OTU table\"  --to-tsv"""
         return cmd
 
     def cmd8(self):
@@ -144,50 +158,72 @@ class UsearchOtuTool(Tool):
                     w.write("\n")
                 else:
                     w.write(line)
-        os.system("""cat otu_table.txt.new|sed -n '2p'|sed 's/#//' >otu_table.xls""")
-        os.system("""cat otu_table.txt.new|sed -n '3,$p'|sort -V |sed 's/\\.0//g' >>otu_table.xls""")
-        cmd = self.qiime_path+"""pick_rep_set.py -i otu_seqids.txt -f meta.fasta -m most_abundant -o otu_reps.fasta"""
+        os.system(
+            """cat otu_table.txt.new|sed -n '2p'|sed 's/#//' >otu_table.xls""")
+        os.system(
+            """cat otu_table.txt.new|sed -n '3,$p'|sort -V |sed 's/\\.0//g' >>otu_table.xls""")
+        cmd = self.qiime_path + \
+            """pick_rep_set.py -i otu_seqids.txt -f meta.fasta -m most_abundant -o otu_reps.fasta"""
         return cmd
 
     def set_output(self):
         self.logger.info("设置输出结果")
         # self.logger.info(self.work_dir+'/otu_table.xls')
         # self.logger.info(self.output_dir+'/otu_table.xls')
-        os.system('rm -rf '+self.output_dir)
-        os.system('mkdir '+self.output_dir)
-        os.link(self.work_dir+'/otu_table.xls', self.output_dir+'/otu_table.xls')
-        self.option('otu_table').set_path(self.output_dir+'/otu_table.xls')
+        os.system('rm -rf ' + self.output_dir)
+        os.system('mkdir ' + self.output_dir)
+        os.link(self.work_dir + '/otu_table.xls',
+                self.output_dir + '/otu_table.xls')
+        self.option('otu_table').set_path(self.output_dir + '/otu_table.xls')
         self.option('otu_table').check()
-        os.link(self.work_dir+'/otu_reps.fasta', self.output_dir+'/otu_reps.fasta')
-        self.option('otu_rep').set_path(self.output_dir+'/otu_reps.fasta')
+        os.link(self.work_dir + '/otu_reps.fasta',
+                self.output_dir + '/otu_reps.fasta')
+        self.option('otu_rep').set_path(self.output_dir + '/otu_reps.fasta')
         self.option('otu_rep').check()
-        os.link(self.work_dir+'/otu_seqids.txt', self.output_dir+'/otu_seqids.txt')
-        self.option('otu_seqids').set_path(self.output_dir+'/otu_seqids.txt')
+        os.link(self.work_dir + '/otu_seqids.txt',
+                self.output_dir + '/otu_seqids.txt')
+        self.option('otu_seqids').set_path(self.output_dir + '/otu_seqids.txt')
         self.option('otu_seqids').check()
-        os.link(self.work_dir+'/otu_table.biom', self.output_dir+'/otu_table.biom')
-        self.option('otu_biom').set_path(self.output_dir+'/otu_table.biom')
+        os.link(self.work_dir + '/otu_table.biom',
+                self.output_dir + '/otu_table.biom')
+        self.option('otu_biom').set_path(self.output_dir + '/otu_table.biom')
         self.option('otu_biom').check()
         self.logger.info("OK,all things done.")
 
     def run(self):
         super(UsearchOtuTool, self).run()
         self.logger.info("将输入文件链接到工作目录")
-        if os.path.exists(self.work_dir+'/meta.fasta'):
-            os.remove(self.work_dir+'/meta.fasta')
-        os.link(self.option("fasta").prop['path'], self.work_dir+'/meta.fasta')
+        if os.path.exists(self.work_dir + '/meta.fasta'):
+            os.remove(self.work_dir + '/meta.fasta')
+        os.link(self.option("fasta").prop[
+                'path'], self.work_dir + '/meta.fasta')
         self.logger.info("OK")
         i = 0
         while i < 8:
             i += 1
-            self.logger.info("开始运行cmd"+str(i))
-            cmd = getattr(self, 'cmd'+str(i))()
-            command = self.add_command('cmd'+str(i), cmd)
+            self.logger.info("开始运行cmd" + str(i))
+            cmd = getattr(self, 'cmd' + str(i))()
+            command = self.add_command('cmd' + str(i), cmd)
             command.run()
             self.wait(command)
             if command.return_code == 0:
-                self.logger.info("运行cmd"+str(i)+"完成")
-            else:
-                self.set_error("cmd"+str(i)+"运行出错!")
-                raise Exception("cmd{}运行出错".format(i))
-        self.set_output()
-        self.end()
+                self.logger.info("运行cmd" + str(i) + "完成")
+            else:  # sehnghe modified 20161215 添加当返回return_code为None时的重运行
+                if command.return_code is None:
+                    command.rerun()
+                    self.wait()
+                    if command.return_code == 0:
+                        self.logger.info("重新运行cmd" + str(i) + "完成")
+                        self.set_error('何胜bug测试，故意报错，如果出现，请删除此句，以及下一句：break')
+                        break
+                    else:
+                        self.set_error("cmd" + str(i) + "重新运行出错!***何胜bug测试，如果出现，请删除星号内文字***")
+                        self.logger.info('Rerun Return Code: {}'.format(command.return_code))
+                        break
+                else:
+                    self.set_error("cmd" + str(i) + "运行出错!")
+                    self.logger.info('Run Return Code: {}'.format(command.return_code))
+                    break
+        else:
+            self.set_output()
+            self.end()
