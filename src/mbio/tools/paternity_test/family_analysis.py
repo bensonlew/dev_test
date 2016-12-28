c33f48f212a659e47224b7e7c76f48a165ced024
moli.zhou <moli.zhou@majorbio.com>
2016-12-28 17:06:45 +0800

    set environ

--- src/mbio/tools/paternity_test/family_analysis.py
+++ src/mbio/tools/paternity_test/family_analysis.py
@@ -80,6 +80,7 @@ class FamilyAnalysisTool(Tool):
 
         self.R_path = 'program/R-3.3.1/bin/'
         self.script_path = Config().SOFTWARE_DIR+'/bioinfo/medical/scripts/'
+        self.set_environ(LD_LIBRARY_PATH=self.config.SOFTWARE_DIR + '/gcc/5.1.0/lib64')
 
     def run_tf(self):
         analysis_cmd = "{}Rscript {}data_analysis.R {}".\
