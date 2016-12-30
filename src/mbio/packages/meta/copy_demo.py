6d10f83e61f181f294782c50cfa7a7c513cfe771
zhouxuan <xuan.zhou@majorbio.com>
2016-12-30 09:07:19 +0800

    增加菌群分型分析copy_demo代码

--- src/mbio/packages/meta/copy_demo.py
+++ src/mbio/packages/meta/copy_demo.py
@@ -133,6 +133,11 @@ class CopyMongo(object):
         hc_heatmap_id_dict = self.copy_collection_with_change('sg_hc_heatmap', change_positions=['otu_id'], update_sg_status=True)
         self.copy_main_details('sg_hc_heatmap_detail', 'hc_id', hc_heatmap_id_dict)
 
+        enterotyping_id_dict = self.copy_collection_with_change('sg_enterotyping', change_positions=['otu_id'], update_sg_status=True)
+        self.copy_main_details('sg_enterotyping_detail', 'enterotyping_id', enterotyping_id_dict)
+        self.copy_main_details('sg_enterotyping_detail_cluster', 'enterotyping_id', enterotyping_id_dict)
+        self.copy_main_details('sg_enterotyping_detail_summary', 'enterotyping_id', enterotyping_id_dict)
+
         self.copy_collection_with_change('sg_valid_sequence_info')
         self.copy_collection_with_change('sg_raw_sequence_info')
 
