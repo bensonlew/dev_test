e311d7505471ab9f503c6167c1699f356fc21e72
qiuping <ping.qiu@majorbio.com>
2017-01-06 17:06:56 +0800

    修改组间差异分析的controller、workflow:包括两组、两样本、多组比较

--- webroot/mainapp/controllers/instant/meta/two_sample.py
+++ webroot/mainapp/controllers/instant/meta/two_sample.py
@@ -5,25 +5,29 @@ import json
 import datetime
 from mainapp.controllers.project.meta_controller import MetaController
 from mainapp.models.mongo.group_stat import GroupStat as G
+from mainapp.models.mongo.meta import Meta
 
 
 class TwoSample(MetaController):
     def __init__(self):
-        super(TwoSample, self).__init__()
+        super(TwoSample, self).__init__(instant=True)
 
     def POST(self):
-        return_info = super(TwoSample, self).POST()  # 初始化出错才会返回
-        if return_info:
-            return return_info
         data = web.input()
         return_result = self.check_options(data)
         if return_result:
             info = {"success": False, "info": '+'.join(return_result)}
             print json.dumps(info)
             return json.dumps(info)
-        self.task_name = 'meta.report.two_sample'
-        self.task_type = 'workflow'  # 可以不配置
-        self.main_table_name = 'TwoSample_' + datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        task_name = 'meta.report.two_sample'
+        task_type = 'workflow'  # 可以不配置
+        meta = Meta()
+        otu_info = meta.get_otu_table_info(data.otu_id)
+        if not otu_info:
+            info = {"success": False, "info": "OTU不存在，请确认参数是否正确！!"}
+            return json.dumps(info)
+        task_info = meta.get_task_info(otu_info['task_id'])
+        main_table_name = 'difference_stat_' + datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
         my_param = dict()
         my_param['otu_id'] = data.otu_id
         my_param['level_id'] = int(data.level_id)
@@ -38,21 +42,41 @@ class TwoSample(MetaController):
         my_param['submit_location'] = data.submit_location
         my_param['task_type'] = 'reportTask'
         params = json.dumps(my_param, sort_keys=True, separators=(',', ':'))
-        self.options = {"otu_file": data.otu_id,
-                        "level": data.level_id,
-                        "test": data.test,
-                        "correction": data.correction,
-                        "ci": data.ci,
-                        "type": data.type,
-                        "sample1": data.sample1,
-                        "sample2": data.sample2,
-                        "methor": data.methor,
-                        "coverage": float(data.coverage),
-                        "params": params
-                        }
-        self.to_file = "meta.export_otu_table_by_level(otu_file)"
-        self.run()
-        return self.returnInfo
+        mongo_data = {
+            "type": 'two_sample',
+            "project_sn": task_info['project_sn'],
+            "task_id": task_info['task_id'],
+            "otu_id": data.otu_id,
+            "name":main_table_name,
+            "level_id": int(data.level_id),
+            "params": params,
+            "status": "start",
+            "created_ts": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+        main_table_id = meta.insert_main_table('sg_species_difference_check', mongo_data)
+        print main_table_id, type(main_table_id)
+        update_info = {str(main_table_id): 'sg_species_difference_check'}
+        options = {
+            "otu_file": data.otu_id,
+            "level": data.level_id,
+            "test": data.test,
+            "correction": data.correction,
+            "ci": data.ci,
+            "type": data.type,
+            "sample1": data.sample1,
+            "sample2": data.sample2,
+            "methor": data.methor,
+            "coverage": float(data.coverage),
+            "params": params,
+            "update_info": json.dumps(update_info),
+            "main_id": str(main_table_id)
+        }
+        to_file = "meta.export_otu_table_by_level(otu_file)"
+        self.set_sheet_data(name=task_name, options=options, main_table_name=main_table_name, module_type=task_type, to_file=to_file)
+        task_info = super(TwoSample, self).POST()
+        task_info['content'] = {'ids': {'id': str(main_table_id), 'name': main_table_name}}
+        print(self.return_msg)
+        return json.dumps(task_info)
 
     def check_options(self, data):
         """
