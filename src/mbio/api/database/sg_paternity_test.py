407fbc668608927da888d0dbfcfd561900fb0012
moli.zhou <moli.zhou@majorbio.com>
2017-01-13 11:31:42 +0800

    modified the table name, and structure

--- src/mbio/api/database/sg_paternity_test.py
+++ src/mbio/api/database/sg_paternity_test.py
@@ -8,6 +8,7 @@ import re
 from biocluster.config import Config
 from pymongo import MongoClient
 import gridfs
+from mainapp.libs.param_pack import param_pack
 
 class SgPaternityTest(Base):
 	'''
@@ -20,54 +21,60 @@ class SgPaternityTest(Base):
 		self.database = self.mongo_client['tsanger_paternity_test']
 
 	@report_check
-	def add_sg_pt_family(self,dad,mom,preg, err,ref_fasta,targets_bedfile,ref_point,fastq_path):
-		family_no = re.search("WQ([1-9].*)-F", dad)
+	def add_sg_task(self,dad,mom,preg,ref_fasta,targets_bedfile,ref_point,fastq_path):
+		# family_no = re.search("WQ([1-9].*)-F", dad)
+		# "name": family_no.group(1)
+		task_id = self.bind_object.id
 		insert_data = {
 			"project_sn": self.bind_object.sheet.project_sn,
-			"task_id": self.bind_object.id,
-			# "member_id": self.bind_object.member_id,
-			"name": family_no.group(1),
+			"task_id": task_id,
+			"member_id": self.bind_object.sheet.member_id,
 			"dad_id": dad,
 			"mom_id": mom,
 			"preg_id": preg,
-			"err_min": err,
+			"created_ts": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
 			"ref_fasta": ref_fasta,
 			"targets_bedfile": targets_bedfile,
 			"ref_point": ref_point,
 			"fastq_path":fastq_path,
-			"status": "end",
-			"created_ts": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 		}
 		try:
-			collection = self.database['sg_pt_family_main']
+			collection = self.database['sg_pt_task']
 			collection.insert_one(insert_data)
 		except Exception as e:
-			self.bind_object.logger.error('导入主表出错：{}'.format(e))
+			self.bind_object.logger.error('导入task表出错：{}'.format(e))
 		else:
-			self.bind_object.logger.info("导入主表成功")
+			self.bind_object.logger.info("导入task表成功")
+		return task_id
 
+	# "status": "end",
 	@report_check
-	def add_pt_task_main(self, task, err_min,flow_id):
-		if task is None:
-			task = self.bind_object.id
-		# flow_id = task + '_' + str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M"))
-		if flow_id is None:
-			flow_id = "%s_%s_%s" % (task, random.randint(1, 10000), random.randint(1, 10000))
+	def add_pt_family(self, task_id, err_min, dedup):
+		params = dict()
+		params['err_min'] = err_min
+		params['dedup'] = dedup
+		name = 'err-' + str(err_min) + '_dedup-'+ str(dedup)
 		insert_data = {
-			"task_id": task,
-			"flow_id": flow_id,
-			"err_min": err_min,
-			"created_ts": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+			"task_id": task_id,
+			"name": name,
+			"created_ts": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
 		}
+
+		collection = self.database['sg_pt_family']
+		new_params = param_pack(params)
+		insert_data["params"] = new_params
+		# collection.insert_data["params"] = params
 		try:
-			collection = self.database['sg_pt_task_main']
-			collection.insert_one(insert_data)
+			flow_id = collection.insert_one(insert_data).inserted_id
+			# collection.insert_one(insert_data)
 		except Exception as e:
 			self.bind_object.logger.error('导入任务主表出错：{}'.format(e))
 		else:
 			self.bind_object.logger.info("导入任务主表成功")
 		return flow_id
 
+	@report_check
 	def add_sg_pt_family_detail(self,file_path,flow_id):
 		self.bind_object.logger.info("开始导入调试表")
 		sg_pt_family_detail = list()
@@ -79,7 +86,7 @@ class SgPaternityTest(Base):
 					continue
 				insert_data = {
 					"task_id": self.bind_object.id,
-					"flow_id": flow_id,
+					"family_id": flow_id,
 					"chrom": line[0],
 					"pos":line[1],
 					"dad_id": line[2],
@@ -139,6 +146,7 @@ class SgPaternityTest(Base):
 			else:
 				self.bind_object.logger.info("导入调试表格成功")
 
+	@report_check
 	def add_pt_figure(self, output_dir,flow_id):
 		self.bind_object.logger.info("图片开始导入数据库")
 		fs = gridfs.GridFS(self.database)
@@ -148,15 +156,16 @@ class SgPaternityTest(Base):
 		preg_percent = fs.put(open(output_dir + '/preg_percent.png', 'r'))
 		update_data = {
 			"task_id": self.bind_object.id,
-			"flow_id": flow_id,
+			"family_id": flow_id,
 			'family_fig': family_fig,
 			'figure1': figure1,
 			'figure2': figure2,
 			'preg_percent': preg_percent
 		}
 		collection = self.database["sg_pt_family_figure"]
-		collection.insert_one({'set': update_data})
+		collection.insert_one(update_data)
 
+	@report_check
 	def add_analysis_tab(self, file_path,flow_id):
 		self.bind_object.logger.info("开始导入分析结果表")
 		sg_pt_family_detail = list()
@@ -168,7 +177,7 @@ class SgPaternityTest(Base):
 					continue
 				insert_data = {
 					"task_id": self.bind_object.id,
-					"flow_id": flow_id,
+					"family_id": flow_id,
 					"dad_id": line[0],
 					"test_pos_n": line[1],
 					"err_pos_n": line[2],
@@ -181,13 +190,14 @@ class SgPaternityTest(Base):
 				}
 				sg_pt_family_detail.append(insert_data)
 			try:
-				collection = self.database['sg_pt_analysis']
+				collection = self.database['sg_pt_family_analysis']
 				collection.insert_many(sg_pt_family_detail)
 			except Exception as e:
 				self.bind_object.logger.error('导入分析结果表格出错：{}'.format(e))
 			else:
 				self.bind_object.logger.info("导入分析结果表格成功")
 
+	@report_check
 	def add_info_detail(self, file_path,flow_id):
 		self.bind_object.logger.info("开始导入信息分析表")
 		sg_pt_family_detail = list()
@@ -199,7 +209,7 @@ class SgPaternityTest(Base):
 					continue
 				insert_data = {
 					"task_id": self.bind_object.id,
-					"flow_id": flow_id,
+					"family_id": flow_id,
 					"preg_id": line[0],
 					"dp_preg": line[1],
 					"percent": line[2],
@@ -207,17 +217,18 @@ class SgPaternityTest(Base):
 					"s_signal": line[4],
 					"mom_id": line[5],
 					"dp_mom": line[6],
-					"mom&preg": line[7]
+					"mom_preg": line[7]
 				}
 				sg_pt_family_detail.append(insert_data)
 			try:
-				collection = self.database['sg_pt_result_info']
+				collection = self.database['sg_pt_family_result_info']
 				collection.insert_many(sg_pt_family_detail)
 			except Exception as e:
 				self.bind_object.logger.error('导入信息分析表格出错：{}'.format(e))
 			else:
 				self.bind_object.logger.info("导入信息分析表格成功")
 
+	@report_check
 	def add_test_pos(self, file_path, flow_id):
 		self.bind_object.logger.info("开始导入位点信息表")
 		sg_pt_family_detail = list()
@@ -229,7 +240,7 @@ class SgPaternityTest(Base):
 					continue
 				insert_data = {
 					"task_id": self.bind_object.id,
-					"flow_id": flow_id,
+					"family_id": flow_id,
 					"test_no": line[0],
 					"chrom": line[1],
 					"dad_geno": line[2],
@@ -239,7 +250,7 @@ class SgPaternityTest(Base):
 				}
 				sg_pt_family_detail.append(insert_data)
 			try:
-				collection = self.database['sg_pt_test_pos']
+				collection = self.database['sg_pt_family_test_pos']
 				collection.insert_many(sg_pt_family_detail)
 			except Exception as e:
 				self.bind_object.logger.error('导入位点信息表格出错：{}'.format(e))
