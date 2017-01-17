34f4ca2ef027a6fcb2a77365dc64a8cb6f4e2497
hesheng <sheng.he@majorbio.com>
2017-01-17 17:16:09 +0800

    工具文件上传问题修改；main.py/mian_wsgi.py整理

--- webroot/main_wsgi.py
+++ webroot/main_wsgi.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 # __author__ = 'guoquan'
 import web
+from biocluster.core.function import hostname
 from mainapp.libs.signature import check_sig
-from mainapp.controllers.pipeline import Pipeline, PipelineState,PipelineLog,PipelineStop,PipelineQueue,PipelineStopPause,PipelinePause
+from mainapp.controllers.pipeline import Pipeline, PipelineState, PipelineLog, PipelineStop, PipelineQueue, PipelineStopPause, PipelinePause
 from mainapp.controllers.filecheck import FileCheck, MultiFileCheck
+from mainapp.controllers.report.download_web_pic import DownloadWebPic
+
+# Meta instant
 from mainapp.controllers.instant.meta.two_group import TwoGroup
 from mainapp.controllers.instant.meta.estimators import Estimators
 from mainapp.controllers.instant.meta.pan_core import PanCore
@@ -14,41 +18,43 @@ from mainapp.controllers.instant.meta.beta.hcluster import Hcluster
 from mainapp.controllers.instant.meta.otu_subsample import OtuSubsample
 from mainapp.controllers.instant.meta.two_sample import TwoSample
 from mainapp.controllers.instant.meta.multiple import Multiple
-from mainapp.controllers.submit.meta.lefse import Lefse
 from mainapp.controllers.instant.meta.est_t_test import EstTTest
-from mainapp.controllers.submit.meta.rarefaction import Rarefaction
 from mainapp.controllers.instant.meta.beta.multi_analysis import MultiAnalysis
 from mainapp.controllers.instant.meta.beta.anosim import Anosim
-from mainapp.controllers.instant.meta.demo_mongodata_copy import DemoMongodataCopy
-from biocluster.core.function import hostname
 from mainapp.controllers.instant.meta.plot_tree import PlotTree
+from mainapp.controllers.instant.meta.corr_network import CorrNetwork
+from mainapp.controllers.instant.meta.pearson_correlation import PearsonCorrelation
+from mainapp.controllers.instant.meta.cluster_analysis import ClusterAnalysis
+from mainapp.controllers.instant.meta.mantel_test import MantelTest
+from mainapp.controllers.instant.meta.hierarchical_clustering_heatmap import HierarchicalClusteringHeatmap
+from mainapp.controllers.instant.meta.enterotyping import Enterotyping
+from mainapp.controllers.instant.meta.demo_mongodata_copy import DemoMongodataCopy
+from mainapp.controllers.instant.meta.convert_level import ConvertLevel
+
+# Meta submit
+from mainapp.controllers.submit.meta.lefse import Lefse
+from mainapp.controllers.submit.meta.rarefaction import Rarefaction
 from mainapp.controllers.submit.meta.randomforest import Randomforest
 from mainapp.controllers.submit.meta.roc import Roc
 from mainapp.controllers.submit.meta.metagenomeseq import Metagenomeseq
 from mainapp.controllers.submit.meta.otunetwork import Otunetwork
-from mainapp.controllers.instant.meta.corr_network import CorrNetwork
 from mainapp.controllers.submit.meta.n_pca import NPca
-from mainapp.controllers.instant.meta.pearson_correlation import PearsonCorrelation
-from mainapp.controllers.instant.meta.cluster_analysis import ClusterAnalysis
 from mainapp.controllers.submit.meta.environmental_regression import EnvironmentalRegression
-from mainapp.controllers.instant.meta.mantel_test import MantelTest
-from mainapp.controllers.instant.meta.hierarchical_clustering_heatmap import HierarchicalClusteringHeatmap
-from mainapp.controllers.instant.meta.enterotyping import Enterotyping
 from mainapp.controllers.submit.meta.function_predict import FunctionPredict
+from mainapp.controllers.submit.meta.meta_sourcetracker import MetaSourcetracker
+
+# Denovo_rna submit
 from mainapp.controllers.submit.denovo_rna.diff_express import DiffExpress
 from mainapp.controllers.submit.denovo_rna.cluster import Cluster
-from mainapp.controllers.instant.denovo_rna.denovo_venn import DenovoVenn
 from mainapp.controllers.submit.denovo_rna.go_enrich_regulate import GoEnrichRegulate
 from mainapp.controllers.submit.denovo_rna.network import Network
-from mainapp.controllers.submit.meta.meta_sourcetracker import MetaSourcetracker
-from mainapp.controllers.instant.meta.convert_level import ConvertLevel
+
+# Denovo_rna instant
+from mainapp.controllers.instant.denovo_rna.denovo_venn import DenovoVenn
+
+
 # web.config.debug = False
 urls = (
-    "/denovo_rna/network", "Network",
-    "/denovo_rna/cluster", "Cluster",
-    "/denovo_rna/diff_express", "DiffExpress",
-    "/denovo_rna/denovo_venn", "DenovoVenn",
-    "/denovo_rna/go_enrich_regulate", "GoEnrichRegulate",
     "/hello", "hello",
     "/filecheck", "FileCheck",
     "/filecheck/multi", "MultiFileCheck",
@@ -60,8 +66,12 @@ urls = (
     "/pipeline/queue", "PipelineQueue",
     "/pipeline/pause", "PipelinePause",
     "/pipeline/stop_pause", "PipelineStopPause",
-    "/meta/convert_level", "ConvertLevel",
+    "/download/report/pdf", "DownloadWebPic",
+    "/download/report/png", "DownloadWebPic",
+
+    # Meta
     "/meta/demo_mongodata_copy", "DemoMongodataCopy",
+    "/meta/convert_level", "ConvertLevel",
     "/meta/estimators", "Estimators",
     "/pipeline/stop_pause", "PipelineStopPause",
     "/meta/pan_core", "PanCore",
@@ -92,12 +102,19 @@ urls = (
     "/meta/hierarchical_clustering_heatmap", "HierarchicalClusteringHeatmap",
     "/meta/enterotyping", "Enterotyping",
     "/meta/function_predict", "FunctionPredict",
-    "/meta/meta_sourcetracker", "MetaSourcetracker"
+    "/meta/meta_sourcetracker", "MetaSourcetracker",
+
+    # denovo_rna
+    "/denovo_rna/network", "Network",
+    "/denovo_rna/cluster", "Cluster",
+    "/denovo_rna/diff_express", "DiffExpress",
+    "/denovo_rna/denovo_venn", "DenovoVenn",
+    "/denovo_rna/go_enrich_regulate", "GoEnrichRegulate"
 )
 
 
 class hello(object):
-    #@check_sig
+    # @check_sig
     def GET(self):
         return "%s %s" % (hostname, web.ctx.homedomain)
 
