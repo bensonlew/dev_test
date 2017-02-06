3b6902a4bf927be25bf2dcbf355136917c4ec6d9
shijin <jin.shi@majorbio.com>
2017-02-06 13:29:19 +0800

    ref rna workflow  and filecheck part

--- src/mbio/workflows/ref_rna/refrna.py
+++ src/mbio/workflows/ref_rna/refrna.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 # __author__ = 'shijin'
-# last_modified by konghualei
+# last_modified by shijin
 """有参转录一键化工作流"""
 
 from biocluster.workflow import Workflow
@@ -12,18 +12,19 @@ import shutil
 class RefrnaWorkflow(Workflow):
     def __init__(self, wsheet_object):
         """
+        有参workflow option参数设置
         """
         self._sheet = wsheet_object
         super(RefrnaWorkflow, self).__init__(wsheet_object)
         options = [
-            {"name": "fq_type", "type": "string"},  # PE OR SE
+            {"name": "fq_type", "type": "string", "default": "PE"},  # PE OR SE
             {"name": "fastq_dir", "type": "infile", 'format': "sequence.fastq_dir"},  # Fastq文件夹
             {"name": "qc_quality", "type": "int", "default": 30},  # 质量剪切中保留的最小质量值
             {"name": "qc_length", "type": "int", "default": 50},  # 质量剪切中保留的最短序列长度
 
-            {"name": "ref_genome", "type": "string"},  # 参考基因组
+            {"name": "ref_genome", "type": "string", "default": "customer_mode"},  # 参考基因组
             {"name": "ref_genome_custom", "type": "infile", "format": "sequence.fasta"},  # 自定义参考基因组
-            {"name": "gff","type": "infile", "format":"ref_rna.reads_mapping.gff"},
+            {"name": "gff", "type": "infile", "format": "ref_rna.reads_mapping.gff"},
             # Ensembl上下载的gff格式文件
 
             {"name": "nr_blast_evalue", "type": "float", "default": 1e-5},  # NR比对e值
@@ -31,13 +32,13 @@ class RefrnaWorkflow(Workflow):
             {"name": "kegg_blast_evalue", "type": "float", "default": 1e-5},  # KEGG注释使用的e值
             {"name": "swissprot_blast_evalue", "type": "float", "default": 1e-5},  # Swissprot比对使用的e值
 
-            {"name": "seq_method", "type": "string"},  # 比对方法，Tophat or Hisat
+            {"name": "seq_method", "type": "string", "default": "Hisat"},  # 比对方法，Tophat or Hisat
             {"name": "strand_specific", "type": "bool", "default": False},
             # 当为PE测序时，是否有链特异性, 默认是False, 无特异性
             {"name": "strand_dir", "type": "string", "default": "None"},
             # 当链特异性时为True时，正义链为forward，反义链为reverse
-            {"name": "map_assess_method", "type": "string", "default": "saturation,duplication,stat,\
-                                                                       distribution,coverage"},
+            {"name": "map_assess_method", "type": "string", "default":
+                "saturation,duplication,distribution,coverage"},
             # 比对质量评估分析
 
             {"name": "assemble_or_not", "type": "bool", "default": True},  # 是否进行拼接
@@ -64,12 +65,12 @@ class RefrnaWorkflow(Workflow):
 
             {"name": "protein_analysis", "type": "string", "default": ""},
             # 蛋白质分析
-            {"name": "tf_database_type", "type": "string", "default" : "iTAK"},  # 转录因子分析所用到的数据库
+            {"name": "tf_database_type", "type": "string", "default": "iTAK"},  # 转录因子分析所用到的数据库
 
         ]
         self.add_option(options)
         self.set_options(self._sheet.options())
-        self.filecheck = self.add_tool("denovo_rna.filecheck.file_denovo")  # 有参重写filecheck方法
+        self.filecheck = self.add_tool("ref_rna.filecheck.filecheck_ref")  # 有参重写filecheck方法
         self.qc = self.add_module("ref_rna.qc.quality_control")
         self.qc_stat_before = self.add_module("ref_rna.qc.qc_stat")
         self.qc_stat_after = self.add_module("ref_rna.qc.qc_stat")
@@ -81,25 +82,46 @@ class RefrnaWorkflow(Workflow):
         self.snp_rna = self.add_module("ref_rna.gene_structure.snp_rna")
         self.seq_abs = self.add_tool("ref_rna.annotation.transcript_abstract")
         self.annotation = self.add_module('denovo_rna.annotation.denovo_annotation')
-        self.altersplicing = self.add_tool('ref_rna.gene_structure.altersplicing.rmats')
+        # self.altersplicing = self.add_tool('ref_rna.gene_structure.altersplicing.rmats')
         self.network = self.add_module("ref_rna.ppinetwork_analysis")
         self.tf = self.add_tool("ref_rna.protein_regulation.TF_predict") 
         self.sample_analysis = self.add_tool("ref_rna.express.sample_analysis")
         self.step.add_steps("qcstat", "mapping", "assembly", "annotation", "express", "map_stat",
-                            "seq_abs","transfactor_analysis","network_analysis","sample_analysis",
+                            "seq_abs", "transfactor_analysis", "network_analysis", "sample_analysis",
                             "altersplicing")
         
     def check_options(self):
         """
         检查选项
         """
+        if not self.option("fq_type") in ["PE", "SE"]:
+            raise OptionError("fq序列类型应为PE或SE")
+        if not self.option("qc_quality") > 0 and not self.option("qc_quality") < 42:
+            raise OptionError("qc中最小质量值超出范围，应在0~42之间")
+        if not self.option("qc_length") > 0:
+            raise OptionError("qc中最小长度超出范围，应大于0")
         if self.option('ref_genome') == "customer_mode":
             if not self.option("ref_genome_custom").is_set or not self.option("gff").is_set:
                 raise OptionError("未设置参考基因组序列文件和gff格式文件")
-        if not self.option("fq_type") in ["PE","SE"]:
-            raise OptionError("fq序列类型")
-        if not self.option("seq_method") in ["Tophat","Hisat"]:
-            raise OptionError("比对软件错误")
+        else:
+            pass  # 此处加上对ref_genome选项的检查
+        if not self.option("nr_blast_evalue") > 0 and not self.option("nr_blast_evalue") < 1:
+            raise OptionError("NR比对的E值超出范围")
+        if not self.option("string_blast_evalue") > 0 and not self.option("string_blast_evalue") < 1:
+            raise OptionError("String比对的E值超出范围")
+        if not self.option("kegg_blast_evalue") > 0 and not self.option("kegg_blast_evalue") < 1:
+            raise OptionError("Kegg比对的E值超出范围")
+        if not self.option("swissprot_blast_evalue") > 0 and not self.option("swissprot_blast_evalue") < 1:
+            raise OptionError("Swissprot比对的E值超出范围")
+        if not self.option("seq_method") in ["Tophat", "Hisat"]:
+            raise OptionError("比对软件应在Tophat与Hisat中选择")
+        for i in self.option('map_assess_method').split(','):
+            if i not in ["saturation", "duplication", "distribution", "coverage"]:
+                raise OptionError("比对质量评估分析没有{}，请检查".format(i))
+        if self.option("assemble_or_not"):
+            if self.option("assemble_method") not in ["cufflinks", "stringtie"]:
+                raise OptionError("拼接软件应在cufflinks和stringtie中选择")
+        # if self.option()
         return True
         
     def set_step(self, event):
@@ -113,8 +135,10 @@ class RefrnaWorkflow(Workflow):
         opts = {
             'fastq_dir': self.option('fastq_dir'),
             'fq_type': self.option('fq_type'),
-            'control_file': self.option('control_file') 
+            'control_file': self.option('control_file')
         }
+        if self.option('ref_genome') == "customer_mode":
+            opts.update({'gff': self.option('gff')})
         if self.option('group_table').is_set:
             opts.update({'group_table': self.option('group_table')})
         self.filecheck.set_options(opts)
@@ -132,9 +156,9 @@ class RefrnaWorkflow(Workflow):
         
     def run_seq_abs(self):
         opts = {
-            "ref_genome_custom" : self.option("ref_genome_custom"),
-            "ref_genome" : self.option("ref_genome"),
-            "ref_genome_gff" : self.option("gff")
+            "ref_genome_custom": self.option("ref_genome_custom"),
+            "ref_genome": self.option("ref_genome"),
+            "ref_genome_gff": self.option("gff")
         }
         self.seq_abs.set_options(opts)
         self.seq_abs.on('start', self.set_step, {'start': self.step.seq_abs})
@@ -176,41 +200,40 @@ class RefrnaWorkflow(Workflow):
             
     def run_mapping(self):
         opts = {
-            "ref_genome_custom" : self.option("ref_genome_custom"),
-            "ref_genome" : self.option("ref_genome"),
-            "mapping_method" : self.option("seq_method").lower(),  # 比对软件
-            "seq_method" : self.option("fq_type"),   # PE or SE
-            "fastq_dir" : self.qc.option("sickle_dir"),
-            "gff" : self.option("gff"),  
-            "assemble_method" : "stringtie"
+            "ref_genome_custom": self.option("ref_genome_custom"),
+            "ref_genome": self.option("ref_genome"),
+            "mapping_method": self.option("seq_method").lower(),  # 比对软件
+            "seq_method": self.option("fq_type"),   # PE or SE
+            "fastq_dir": self.qc.option("sickle_dir"),
+            "gff": self.option("gff"),
+            "assemble_method": "stringtie"
         }
         self.mapping.set_options(opts)
-        self.mapping.on("end",self.set_output,"mapping")
+        self.mapping.on("end", self.set_output, "mapping")
         self.mapping.run()
      
     def run_assembly(self):
         self.logger.info("开始运行拼接步骤")
         if self.option("ref_genome") == "customer_mode":
-            #gtf_path = self.option("gff").prop["path"][:-4] + ".gtf"
-            gtf_path=self.option("gff").gff_to_gtf()
+            # gtf_path = self.option("gff").prop["path"][:-4] + ".gtf"
+            gtf_path = self.option("gff").gff_to_gtf()
         else:
             gtf_path = ""
         if os.path.exists(gtf_path):
             self.logger.info("gtf存在")
             self.logger.info("gtf的路径为:" + gtf_path)
-        #self.logger.info(self.mapping.option("bam_output"))
+        # self.logger.info(self.mapping.option("bam_output"))
         if self.option("strand_specific") != "null":
             fr_strand = self.option("strand_dir")
         else:
-            fr_strand = "fr-unstranded" #鏃犻摼鐗瑰紓鎬�
+            fr_strand = "fr-unstranded"
         opts = {
-            "ref_gtf" : gtf_path,
-            "sample_bam_dir" : self.mapping.option("bam_output"),
-            "ref_fa" : self.option("ref_genome_custom"),
-            "assemble_method" : self.option("assemble_method"),
-            "fr_stranded" : fr_strand
+            "ref_gtf": gtf_path,
+            "sample_bam_dir": self.mapping.option("bam_output"),
+            "ref_fa": self.option("ref_genome_custom"),
+            "assemble_method": self.option("assemble_method"),
+            "fr_stranded": fr_strand
         }
-        """閾剧壒寮傛��"""
         """
         if self.option("strand_specific"):
             if not self.option("strand_dir"):
@@ -232,28 +255,22 @@ class RefrnaWorkflow(Workflow):
         
     def run_snp(self):
         opts = {
-            "ref_genome_custom" : self.option("ref_genome_custom"),
-            "ref_genome" : self.option("ref_genome"),
-            "seq_method" : self.option("fq_type"),
-            "fastq_dir" : self.qc.option("sickle_dir")
+            "ref_genome_custom": self.option("ref_genome_custom"),
+            "ref_genome": self.option("ref_genome"),
+            "seq_method": self.option("fq_type"),
+            "fastq_dir": self.qc.option("sickle_dir")
         }
         self.snp_rna.set_options(opts)
         self.snp_rna.on("end", self.set_output, "snp_rna")
         self.snp_rna.run()
         
-    def run_map_assess(self):
-        if self.option("ref_genome") == "customer_mode":
-            self.option("gff").gff_to_bed()
-            self.logger.info("gff杞崲瀹屾垚锛�")
-            bed_path = self.option("gff").prop["path"][:-4] + ".bed"
-        else:
-            bed_path = ""
-        opts ={
-            "bed" : bed_path,
-            "bam" : self.mapping.option("bam_output") 
+    def run_map_assess(self):  # 此处加上stat
+        opts = {
+            "bed": bed_path,
+            "bam": self.mapping.option("bam_output")
         }
         self.map_qc.set_options(opts)
-        self.map_qc.on("end",self.set_output,"map_qc")
+        self.map_qc.on("end", self.set_output, "map_qc")
         self.map_qc.run()
     
     def run_exp(self):
@@ -261,9 +278,8 @@ class RefrnaWorkflow(Workflow):
         self.logger.info("寮�濮嬭〃杈鹃噺鍒嗘瀽!")
         if self.option("gtf_type") == "ref":
             if self.option("ref_genome") == "customer_mode":
-                #if not os.path.exists(gtf_path):
                 gtf_path = self.option("gff").gff_to_gtf()
-                #gtf_path = self.option("gff").prop["path"][:-4] + ".gtf"
+                # gtf_path = self.option("gff").prop["path"][:-4] + ".gtf"
             else:
                 gtf_path = ""
         elif self.option("gtf_type") == "new_transcripts":
@@ -277,16 +293,16 @@ class RefrnaWorkflow(Workflow):
             self.logger.info("" + gtf_path)
         self.logger.info(self.mapping.option("bam_output").prop["path"])
         opts = {
-            "fq_type" : self.option("fq_type"),
-            "ref_gtf" : gtf_path,
-            "gtf_type" : "ref",
-            "express_method" : self.option("express_method"),
-            "sort_type" : "pos",
-            "sample_bam" : self.mapping.option("bam_output"),
-            "strand_specific" : self.option("strand_specific"),
-            "control_file" : self.option("control_file"),
-            "edger_group" : self.option("group_table"),
-            "change_sample_name" : self.option("change_sample_name"),
+            "fq_type": self.option("fq_type"),
+            "ref_gtf": gtf_path,
+            "gtf_type": "ref",
+            "express_method": self.option("express_method"),
+            "sort_type": "pos",
+            "sample_bam": self.mapping.option("bam_output"),
+            "strand_specific": self.option("strand_specific"),
+            "control_file": self.option("control_file"),
+            "edger_group": self.option("group_table"),
+            "change_sample_name": self.option("change_sample_name"),
             "feature_id": self.option("feature_id"),
             "gname": "group",
             "method": self.option("diff_method"),
@@ -296,12 +312,12 @@ class RefrnaWorkflow(Workflow):
         if self.option("strand_specific"):
             if not self.option("strand_dir"):
                 opts.update({
-                    "strand_dir" : self.option("strand_dir") 
+                    "strand_dir": self.option("strand_dir")
                 })
             else:
                 raise Exception("閾剧壒寮傛�ф椂璇疯緭鍏ユ璐熼摼锛�")
         self.exp.set_options(opts)
-        self.exp.on("end",self.set_output, "express")
+        self.exp.on("end", self.set_output, "express")
         self.exp.on('start', self.set_step, {'start': self.step.express})
         self.exp.on('end', self.set_step, {'end': self.step.express})
         self.exp.run()
@@ -336,11 +352,11 @@ class RefrnaWorkflow(Workflow):
     def run_network(self):
         if self.option("ref_genome") != "customer_mode":
             opts = {
-                "diff_exp" : self.exp.option("diff_list"),  #
-                "species_list" : "",
-                "species" : self.option("ref_genome"),
+                "diff_exp": self.exp.option("diff_list"),  #
+                "species_list": "",
+                "species": self.option("ref_genome"),
                 "combine_score": self.option("combine_score"),
-                "logFC" : self.option("logFC")
+                "logFC": self.option("logFC")
             }
             self.network.set_options(opts)
             self.network.on("end", self.set_output, "network_analysis")
@@ -352,10 +368,10 @@ class RefrnaWorkflow(Workflow):
         self.diff_fpkm = self.output_dir+"express/merge"+self.option("express_method").lower()+"_express/" \
                                                                                                "fpkm.txt"
         opts = {
-            "diff_fpkm" : self.diff_fpkm,
-            "distance_method" : self.option("distance_method"),
-            "method" : self.option("clsuter_method"),
-            "lognorm" : self.option("lognorm")
+            "diff_fpkm": self.diff_fpkm,
+            "distance_method": self.option("distance_method"),
+            "method": self.option("cluster_method"),
+            "lognorm": self.option("lognorm")
         }
         self.sample_analysis.set_options(opts)
         self.sample_analysis.on("end", self.set_output, "sample_analysis")
@@ -365,22 +381,22 @@ class RefrnaWorkflow(Workflow):
     
     def run_altersplicing(self):
         if self.option("ref_genome") == "customer_mode":
-            #gtf_path = self.option("gff").prop["path"][:-4] + ".gtf"
-            #if not os.path.exists(gtf_path):
-            gtf_path=self.option("gff").gff_to_gtf()
+            # gtf_path = self.option("gff").prop["path"][:-4] + ".gtf"
+            # if not os.path.exists(gtf_path):
+            gtf_path = self.option("gff").gff_to_gtf()
         else:
             gtf_path = ""
         opts = {
             "sequencing_library_type": self.option("sequencing_library_type"),
-            "whether_to_find_novel_splice_sites" : self.option("whether_to_find_novel_splice_sites"),
-            "genome_annotation_file" : gtf_path,
-            "group_table" : self.option("gtf_path"),
-            "control_file" : self.option("control_file")
+            "whether_to_find_novel_splice_sites": self.option("whether_to_find_novel_splice_sites"),
+            "genome_annotation_file": gtf_path,
+            "group_table": self.option("gtf_path"),
+            "control_file": self.option("control_file")
         }
         if self.option("fq_type") == "PE":
-            opts.update({"sequencing_type" : "paired"})
+            opts.update({"sequencing_type": "paired"})
         else:
-            opts.update({"sequencing_type" : "single"})
+            opts.update({"sequencing_type": "single"})
         self.altersplicing.set_options(opts)
         self.altersplicing.on("end", self.set_output, "altersplicing")
         self.altersplicing.on('start', self.set_step, {'start': self.step.altersplicing})
@@ -389,8 +405,8 @@ class RefrnaWorkflow(Workflow):
 
     def run_tf(self):
         opts = {
-            "diff_gene_list" : self.exp.option("diff_list"),
-            "database" : self.option("tf_database_type")
+            "diff_gene_list": self.exp.option("diff_list"),
+            "database": self.option("tf_database_type")
         }
         self.tf.set_options(opts)
         self.tf.on("end", self.set_output, "tf")
@@ -479,22 +495,24 @@ class RefrnaWorkflow(Workflow):
         首先进行filecheck，然后运行qc module，结束后运行比对模块，根据self.option("assemble_or_not")选择是否运行拼接
         :return:
         """
-        self.filecheck.on('end', self.run_qc)
-        self.qc.on('end',self.run_mapping)
-        self.mapping.on("end",self.assembly)
-        self.exp.on("end",self.end)
+        self.filecheck.on('end', self.end)
+        # self.filecheck.on('end', self.run_qc)
+        # self.qc.on('end', self.run_mapping)
+        # self.mapping.on("end", self.run_assembly)
+        # self.assembly.on("end", self.run_exp)
+        # self.exp.on("end", self.end)
 
         # self.qc.on('end', self.run_seq_abs)
-        #self.annotation.on('end', self.run_mapping)
-        #self.qc.on('end',self.run_snp)
-        #self.seq_abs.on("end",self.run_annotation)
+        # self.annotation.on('end', self.run_mapping)
+        # self.qc.on('end',self.run_snp)
+        # self.seq_abs.on("end",self.run_annotation)
 
-        #self.on_rely([self.exp,self.annotation],self.exp_diff)
+        # self.on_rely([self.exp,self.annotation],self.exp_diff)
 
-        #self.sample_analysis.on("end",self.end)
-        #self.exp_diff.on("end",self.run_tf)
-        #self.exp_diff.on("end",self.run_network)
-        #self.on_rely([self.tf,self.network,self.sample_analysis,self.snp_rna,self.altersplicing],self.end)
+        # self.sample_analysis.on("end",self.end)
+        # self.exp_diff.on("end",self.run_tf)
+        # self.exp_diff.on("end",self.run_network)
+        # self.on_rely([self.tf,self.network,self.sample_analysis,self.snp_rna,self.altersplicing],self.end)
         self.run_filecheck()
         super(RefrnaWorkflow, self).run()
         
