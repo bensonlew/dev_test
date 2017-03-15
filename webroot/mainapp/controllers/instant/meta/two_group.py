3d8b0120644592ab6a88918da5ece50dfdf519a5
xuanhongdong <hongdong.xuan@majorbio.com>
2017-03-15 09:33:33 +0800

    two_group controller update error info

--- webroot/mainapp/controllers/instant/meta/two_group.py
+++ webroot/mainapp/controllers/instant/meta/two_group.py
@@ -108,6 +108,8 @@ class TwoGroup(MetaController):
         if float(data.coverage) not in [0.90, 0.95, 0.98, 0.99, 0.999]:
             success.append('置信区间的置信度coverage不在范围值内')
         table_dict = json.loads(data.group_detail)
+        if len(table_dict) != 2 or data.group_id == 'all':     #modified by hongdongxuan 20170313
+            success.append("该分析中分组方案的分组类别必须等于2！")
         if not isinstance(table_dict, dict):
             success.append("传入的table_dict不是一个字典")
         return success
