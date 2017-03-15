10472ac4486e625e5ec5d67ba4d359cf88281e96
xuanhongdong <hongdong.xuan@majorbio.com>
2017-03-15 09:30:16 +0800

    otunetwork controller update error info

--- webroot/mainapp/controllers/submit/meta/otunetwork.py
+++ webroot/mainapp/controllers/submit/meta/otunetwork.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # __author__ = 'xuanhongdong'
+# last modify 20170310
 import web
 import json
 import datetime
@@ -23,9 +24,19 @@ class Otunetwork(MetaController):
         if int(data.level_id) not in range(1, 10):
             info = {"success": False, "info": "level{}不在规定范围内{}".format(data.level_id)}
             return json.dumps(info)
+        if int(data.level_id) in [1, 2]:
+            info = {'success': False, 'info': 'OTU表中的物种数目太少，不能进行该分析，请选择Phylum以下的分类水平！'}
+            return json.dumps(info)
         group_detail = json.loads(data.group_detail)
         if not isinstance(group_detail, dict):
-            success.append("传入的group_detail不是一个字典")
+            info = {'success': False, 'info': '传入的group_detail不是一个字典！'}
+            return json.dumps(info)
+        sample_num = 0
+        for key in group_detail:
+            sample_num += len(group_detail[key])
+        if sample_num < 2:
+            info = {'success': False, 'info': '样本的总个数少于2个，不能进行共现性网络分析！'}
+            return json.dumps(info)
         otu_info = Meta().get_otu_table_info(data.otu_id)
         if not otu_info:
             info = {"success": False, "info": "OTU不存在，请确认参数是否正确！!"}
@@ -40,7 +51,7 @@ class Otunetwork(MetaController):
             'group_id': data.group_id,
             'group_detail': group_detail_sort(data.group_detail),
             'submit_location': data.submit_location,
-            'task_type': 'reportTask'
+            'task_type': data.task_type
         }
         params = json.dumps(params_json, sort_keys=True, separators=(',', ':'))
         if data.group_id == 'all':
