eaea10b306bbd909050cbf46bcf486b4dbdfd6f9
xuanhongdong <hongdong.xuan@majorbio.com>
2017-03-22 15:47:54 +0800

    multi_analysis.py add file path

--- webroot/mainapp/controllers/instant/meta/beta/multi_analysis.py
+++ webroot/mainapp/controllers/instant/meta/beta/multi_analysis.py
@@ -165,8 +165,8 @@ class MultiAnalysis(MetaController):
         if data.analysis_type == 'plsda':
             to_file.append('meta.export_group_table_by_detail(group_file)')
             options['group_file'] = data.group_id
-        self.set_sheet_data(name=task_name, options=options, main_table_name=main_table_name,
-                            module_type=task_type, to_file=to_file)
+        self.set_sheet_data(name=task_name, options=options, main_table_name=main_table_name.strip().split("_")[0] + '/' + main_table_name,
+                            module_type=task_type, to_file=to_file) # modified by hongdongxuan 20170322 在main_table_name前面加上文件输出的文件夹名
         task_info = super(MultiAnalysis, self).POST()
         task_info['content'] = {
             'ids': {
