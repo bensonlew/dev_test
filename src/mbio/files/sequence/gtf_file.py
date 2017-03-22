4f89773635e2692916839658575012c623933c06
jinlinfang <linfang.jin@majorbio.com>
2017-03-22 14:55:18 +0800

    gff文件相关类文件提交（还未写完）

---
+++ src/mbio/files/sequence/gtf_file.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# __author__ = fiona
+# time: 2017/3/10 18:08
+
+
+import re, os, Bio, argparse, sys, fileinput, urllib2
+import Bio
+import subprocess
+from bs4 import BeautifulSoup
+from file import File
+from gff3_file import Gff3File
+
+
+
+
+class GtfFile(File):
+	def __init__(self):
+		pass
+	
+	def check(self):
+		pass
+	
+	def parse(self):
+		pass
