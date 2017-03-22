411ed132a3624e691f0ede290edf10d26358e901
xuanhongdong <hongdong.xuan@majorbio.com>
2017-03-22 15:19:31 +0800

    otunetwork.py add file path

--- webroot/mainapp/controllers/submit/meta/otunetwork.py
+++ webroot/mainapp/controllers/submit/meta/otunetwork.py
@@ -82,8 +82,8 @@ class Otunetwork(MetaController):
             "network_id": str(main_table_id)
         }
         to_file = ["meta.export_otu_table_by_detail(otutable)", "meta.export_group_table_by_detail(grouptable)"]
-        self.set_sheet_data(name=task_name, options=options, main_table_name=main_table_name,
-                            module_type=task_type, to_file=to_file)
+        self.set_sheet_data(name=task_name, options=options, main_table_name="OTUnetwork/" + main_table_name,
+                            module_type=task_type, to_file=to_file) # modified by hongdongxuan 20170322 在main_table_name前面加上文件输出的文件夹名
         task_info = super(Otunetwork, self).POST()
         # print "+++++..."
         task_info['content'] = {
