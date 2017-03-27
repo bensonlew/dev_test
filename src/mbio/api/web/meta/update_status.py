77235ac595f846875036cc7afe1e0667a12b98e2
xuanhongdong <hongdong.xuan@majorbio.com>
2017-03-27 14:04:29 +0800

    添加output文件夹上一层的文件说明，以及report_result的说明文件

--- src/mbio/api/web/meta/update_status.py
+++ src/mbio/api/web/meta/update_status.py
@@ -8,6 +8,7 @@ import re
 import gevent
 import urllib2
 import sys
+import os
 from bson.objectid import ObjectId
 from biocluster.wpm.log import Log
 from biocluster.config import Config
@@ -31,6 +32,8 @@ class UpdateStatus(Log):
         self.mongodb = self._mongo_client[Config().MONGODB]
 
     def get_post_data(self):
+        ana_dir = {}
+        report_dir_des = {}
         workflow_id = self.data["content"]["stage"]["task_id"]
         my_id = re.split('_', workflow_id)
         my_id.pop(-1)
@@ -43,7 +46,22 @@ class UpdateStatus(Log):
         if 'files' in self.data['content']:
             content['files'] = self.data["content"]["files"]
         if 'dirs' in self.data['content']:
-            content['dirs'] = self.data['content']['dirs']
+            content['dirs'] = self.data['content']['dirs']  # add 15 lines by hongdongxuan 20170327, 遍历dirs中路径最短的进行处理
+            path_len = len(content['dirs'][0]['path'].rstrip('/').split("/"))
+            for m in content['dirs']:
+                if len(m['path'].rstrip('/').split("/")) < path_len:
+                    output_dir = os.path.dirname(m['path'].rstrip('/'))
+                    ana_dir = {'path': output_dir, "size": "", "description": m['description'], "format": ""}
+                    report_dir = os.path.dirname(output_dir.rstrip("/"))
+                    report_dir_des = {'path': report_dir, "size": "", "description": "\u4ea4\u4e92\u5206\u6790\u7ed3\u679c\u6587\u4ef6\u5939", "format": ""}
+                else:
+                    output_dir = os.path.dirname(content['dirs'][0]['path'].rstrip('/'))
+                    ana_dir = {'path': output_dir, "size": "", "description": content['dirs'][0]['description'],
+                               "format": ""}
+                    report_dir = os.path.dirname(output_dir.rstrip("/"))
+                    report_dir_des = {'path': report_dir, "size": "", "description": "\u4ea4\u4e92\u5206\u6790\u7ed3\u679c\u6587\u4ef6\u5939", "format": ""}
+            content['dirs'].append(ana_dir)
+            content['dirs'].append(report_dir_des)
         data['content'] = json.dumps(content, cls=CJsonEncoder)
         return urllib.urlencode(data)
 
