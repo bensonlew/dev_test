48ec9e1662fd67eaf85a36a83cddf8a55b0a4503
xuanhongdong <hongdong.xuan@majorbio.com>
2017-03-31 15:45:18 +0800

    修改了lefse报错信息为乱码的情况

--- webroot/mainapp/controllers/submit/meta/lefse.py
+++ webroot/mainapp/controllers/submit/meta/lefse.py
@@ -98,11 +98,9 @@ class Lefse(MetaController):
         if not isinstance(group_detail, dict):
             success.append("传入的group_detail不是一个字典")
         if data.group_id == 'all':     #modified by hongdongxuan 20170313
-            info = {"success": False, "info": "分组方案至少选择两个分组！"}
-            return json.dumps(info)
-        if len(group_detail) < 2:
-            info = {"success": False, "info": "请选择至少两组以上的分组!"}
-            return json.dumps(info)
+            success.append("分组方案至少选择两个分组！")
+        elif len(group_detail) < 2:
+            success.append("请选择至少两组以上的分组方案!")
         if data.second_group_detail != '':
             second_group_detail = json.loads(data.second_group_detail)
             first = 0
