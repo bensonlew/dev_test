ee8bd20cbd41fc00b70c7beed5bce80b1e733dac
moli.zhou <moli.zhou@majorbio.com>
2017-04-14 15:26:16 +0800

    修改内存

--- src/mbio/tools/paternity_test/family2bam.py
+++ src/mbio/tools/paternity_test/family2bam.py
@@ -64,7 +64,7 @@ class Family2bamAgent(Agent):
         :return:
         """
         self._cpu = 10
-        self._memory = '150G'
+        self._memory = '50G'
 
     def end(self):
         result_dir = self.add_upload_dir(self.output_dir)
