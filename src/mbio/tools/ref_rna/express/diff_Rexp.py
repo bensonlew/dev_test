5c7a0ee5e6f6cb0fa74a1dbbbfdc08b915ff5d8c
konghualei <hualei.kong@majorbio.com>
2017-04-18 11:22:20 +0800

    update diff_Rexp

--- src/mbio/tools/ref_rna/express/diff_Rexp.py
+++ src/mbio/tools/ref_rna/express/diff_Rexp.py
@@ -3,8 +3,8 @@
 from biocluster.agent import Agent
 from biocluster.tool import Tool
 from biocluster.core.exceptions import OptionError
-from mbio.packages.ref_rna.get_diff_matrix import *
-from mbio.packages.ref_rna.DE_stat import *
+from mbio.packages.ref_rna.express.get_diff_matrix import *
+from mbio.packages.ref_rna.express.DE_stat import *
 import os
 import re
 import itertools
