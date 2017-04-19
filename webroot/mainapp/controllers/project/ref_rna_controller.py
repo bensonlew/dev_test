bdb678e0c4339e5317c7ccd040270c85eed3ed54
qindanhua <qdh504f@126.com>
2017-04-19 16:46:51 +0800

    ref_controller

--- webroot/mainapp/controllers/project/ref_rna_controller.py
+++ webroot/mainapp/controllers/project/ref_rna_controller.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
-# __author__ = 'moli.zhou'
-
-from meta_controller import MetaController
+# __author__ = 'qiuping'
 import web
+import random
+from ..core.basic import Basic
+from mainapp.libs.signature import check_sig
+from mainapp.models.mongo.meta import Meta
+from mainapp.models.workflow import Workflow
+from meta_controller import MetaController
+from biocluster.config import Config
 
 
 class RefRnaController(MetaController):
     def __init__(self, instant=False):
         super(RefRnaController, self).__init__(instant)
-        # self.mongodb = Config().MONGODB + '_ref_rna'
+        self.mongodb = Config().MONGODB + '_ref_rna'
 
     def _update_status_api(self):
         """
-        根据client决定接口api为denovo.update_status/denovo.tupdate_status
+        根据client决定接口api为ref_rna.update_status/ref_rna.tupdate_status
         """
         data = web.input()
         client = data.client if hasattr(data, "client") else web.ctx.env.get('HTTP_CLIENT')
@@ -20,3 +25,13 @@ class RefRnaController(MetaController):
             return 'ref_rna.update_status'
         else:
             return 'ref_rna.tupdate_status'
+
+    @check_sig
+    def POST(self):
+        workflow_client = Basic(data=self.sheet_data, instant=self.instant)
+        try:
+            run_info = workflow_client.run()
+            self._return_msg = workflow_client.return_msg
+            return run_info
+        except Exception, e:
+            return {"success": False, "info": "运行出错: %s" % e }
