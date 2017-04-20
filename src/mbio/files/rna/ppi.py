5b64d9cde6641c421cec3fda5ca217c26609af22
xuanhongdong <hongdong.xuan@majorbio.com>
2017-04-20 15:02:03 +0800

    ppi input file

--- src/mbio/files/rna/ppi.py
+++ src/mbio/files/rna/ppi.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
-
 # __author__ = 'hongdongxuan'
-# time: 2017/
+# time: 2017.04.20
 
-import re
 from biocluster.iofile import File
 from biocluster.core.exceptions import FileError
 
 
 class PpiFile(File):
     """
-    as文件夹格式
+    ppi文件夹格式
     """
     def __init__(self):
         super(PpiFile, self).__init__()
 
     def check(self):
-        if super(PpiFile, self).check():
+        super(PpiFile, self).check()
+        with open(self.prop["path"], "r") as r:
+            line = r.readlines()[0]
+            line = line.strip().split("\t")
+            if str(line[0]) != "gene_id":
+                raise FileError("基因列表中第一行第一个字段必须为gene_id")
             return True
+
+# if __name__ == "__main__":
+#     a = PpiFile()
+#     a.set_path("/mnt/ilustre/users/sanger-dev/sg-users/xuanhongdong/test/9606.txt")
+#     a.check()
