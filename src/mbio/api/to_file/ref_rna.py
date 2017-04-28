532d5ab0a422325ba4516b3af7de2fec6cd5a022
zengjing <jing.zeng@majorbio.local>
2017-04-28 08:42:34 +0800

    ref_rna.py里增加导出blast_table的函数

--- src/mbio/api/to_file/ref_rna.py
+++ src/mbio/api/to_file/ref_rna.py
@@ -3,6 +3,10 @@
 import os
 from biocluster.config import Config
 from bson.objectid import ObjectId
+import types
+import json
+import re
+from types import StringTypes
 
 
 # client = Config().mongo_client
@@ -26,10 +30,72 @@ def export_gene_list(data, option_name, dir_path, bind_obj=None):
     return gene_list_path
 
 
+def export_blast_table(data, option_name, dir_path, bind_obj=None):
+    """
+    获取blast结果
+    """
+    db = Config().mongo_client[Config().MONGODB + "_ref_rna"]
+    nr_table_path = os.path.join(dir_path, "nr_{}.xls".format(option_name))
+    gene_nr_table_path = os.path.join(dir_path, "gene_nr_{}.xls".format(option_name))
+    sw_table_path = os.path.join(dir_path, "swissprot_{}.xls".format(option_name))
+    gene_sw_table_path = os.path.join(dir_path, "gene_swissprot_{}.xls".format(option_name))
+    blast_collection = db["sg_annotation_blast"]
+    blast_result = blast_collection.find({"stat_id": ObjectId(data)})
+    if not blast_result.count():
+        raise Exception("stat_id:{}在sg_annotation_blast表中未找到".format(data))
+    for result in blast_result:
+        blast_id = result["_id"]
+    collection = db["sg_annotation_blast_detail"]
+    results = collection.find({"blast_id": blast_id})
+    with open(nr_table_path, "wb") as w1, open(gene_nr_table_path, "wb") as w2, open(sw_table_path, "wb") as w3, open(gene_sw_table_path, "wb") as w4:
+        header = "Score\tE-Value\tHSP-Len\tIdentity-%\tSimilarity-%\tQuery-Name\tQ-Len\tQ-Begin\t"
+        header += "Q-End\tQ-Frame\tHit-Name\tHit-Len\tHsp-Begin\tHsp-End\tHsp-Frame\tHit-Description\n"
+        w1.write(header)
+        w2.write(header)
+        w3.write(header)
+        w4.write(header)
+        for result in results:
+            db = result["database"]
+            anno_type = result["anno_type"]
+            seq_type = result["seq_type"]
+            score = result["score"]
+            evalue = result["e_value"]
+            hsp_len = result["hsp_len"]
+            identity = result["identity_rate"]
+            similarity = result["similarity_rate"]
+            query_id = result["query_id"]
+            hit_name = result["hit_name"]
+            description = result["description"]
+            q_len = result["q_len"]
+            q_begin = result["q_begin"]
+            q_end = result["q_end"]
+            q_frame = result["q_frame"]
+            hit_len = result["hit_len"]
+            hsp_begin = result["hsp_begin"]
+            hsp_end = result["hsp_end"]
+            hsp_frame = result["hsp_frame"]
+            line = str(score) + "\t" + str(evalue) + "\t" + str(hsp_len) + "\t" + str(identity) + "\t"
+            line += str(similarity) + "\t" + query_id + "\t" + q_len + "\t" + q_begin + "\t" + q_end + "\t" + q_frame + "\t"
+            line += hit_name + "\t"+ hit_len + "\t" + hsp_begin + "\t" + hsp_end + "\t" + hsp_frame + "\t" + description + "\n"
+            if seq_type == "new":
+                if db == "nr":
+                    if anno_type == "transcript":
+                        w1.write(line)
+                    if anno_type == "gene":
+                        w2.write(line)
+                if db == "swissprot":
+                    if anno_type == "transcript":
+                        w3.write(line)
+                    if anno_type == "gene":
+                        w4.write(line)
+    paths = ",".join([nr_table_path, gene_nr_table_path, sw_table_path, gene_sw_table_path])
+    return paths
+
+
 def export_go_list(data, option_name, dir_path, bind_obj=None):
     db = Config().mongo_client[Config().MONGODB + "_ref_rna"]
-    go_list_path = os.path.join(dir_path, "GO.list")
-    bind_obj.logger.debug("正在导出%sgo列表:%s" % (option_name, go_list_path))
+    go_list = os.path.join(dir_path, "GO.list")
+    bind_obj.logger.debug("正在导出%sgo列表:%s" % (option_name, go_list))
     geneset_collection = db["sg_geneset"]
     task_id = geneset_collection.find_one({"_id": ObjectId(data)})["task_id"]
     my_result = db["sg_annotation_go"].find_one({"task_id": task_id})
@@ -40,12 +106,12 @@ def export_go_list(data, option_name, dir_path, bind_obj=None):
     results = collection.find({"go_id": ObjectId(go_id)})
     if not results:
         raise Exception("生成gos_list出错：annotation_id:{}在sg_annotation_gos_list中未找到！".format(ObjectId(go_id)))
-    with open(go_list_path, "wb") as w:
+    with open(go_list, "wb") as w:
         for result in results:
             gene_id = result["gene_id"]
             go_list = result["gos_list"]
             w.write(gene_id + "\t" + go_list + "\n")
-    return go_list_path
+    return go_list
 
 
 def export_kegg_table(data, option_name, dir_path, bind_obj=None):
@@ -55,15 +121,14 @@ def export_kegg_table(data, option_name, dir_path, bind_obj=None):
     geneset_collection = db["sg_geneset"]
     geneset_result = geneset_collection.find_one({"_id": ObjectId(data)})
     task_id = geneset_result["task_id"]
-    # geneset_type = geneset_result["type"]
+    geneset_type = geneset_result["type"]
     my_result = db["sg_annotation_kegg"].find_one({"task_id": task_id})
     kegg_id = my_result["_id"]
     if not my_result:
         raise Exception("意外错误，annotation_kegg_id:{}在sg_annotation_kegg中未找到！".format(kegg_id))
     with open(kegg_path, 'wb') as w:
         w.write('#Query\tKO_ID(Gene id)\tKO_name(Gene name)\tHyperlink\tPaths\n')
-        # results = db['sg_annotation_kegg_table'].find({'$and': [{'kegg_id': kegg_id}, {'type': geneset_type}]})
-        results = db['sg_annotation_kegg_table'].find({'kegg_id': kegg_id})
+        results = db['sg_annotation_kegg_table'].find({'$and': [{'kegg_id': kegg_id}, {'type': geneset_type}]})
         if not results:
             raise Exception("生成kegg_table出错：kegg_id:{}在sg_annotation_kegg_table中未找到！".format(ObjectId(kegg_id)))
         for result in results:
@@ -127,7 +192,8 @@ def export_diff_express(data, option_name, dir_path, bind_obj=None):  # 需要�
                 log = result["log2FC({}/{})".format(compare_name, name)]
                 pval = result["Pvalue"]
                 fdr = result["Fdr"]
-            w.write(gene_id + '\t' + str(name_count) + '\t' + str(compare_count) + '\t' + str(name_fpkm) + '\t' + str(compare_fpkm) + '\t' + str(log) + '\t' + str(pval) + '\t' + str(fdr) + '\t' + significant + '\t' + regulate + '\n')
+            w.write(gene_id + '\t' + str(name_count) + '\t' + str(compare_count) + '\t' + str(name_fpkm) + '\t' + str(compare_fpkm) + '\t' + str(log) + '\t' + str(pval) + '\t' + str(fdr) +
+'\t' + significant + '\t' + regulate + '\n')
     return diff_express
 
 
