68f5f84ec1e544e09d9a267dca75c58098d4c3c6
shijin <jin.shi@majorbio.com>
2017-05-03 14:13:37 +0800

    上传表格文件的注释模块

--- src/mbio/modules/rna/anno_upload.py
+++ src/mbio/modules/rna/anno_upload.py
@@ -9,25 +9,21 @@ from biocluster.module import Module
 
 class AnnoUploadModule(Module):
     """
-    根据kegg、go的upload_table文件和cog的table文件
+    根据kegg、go的upload_table文件和cog的table文件,进行上传表格文件的注释
     version 1.0
     """
     def __init__(self, work_id):
         super(AnnoUploadModule, self).__init__(work_id)
         options = [
-            {"name": "nr_align_dir", "type": "infile", "format": "align.blast.blast_xml_dir"},
-            {"name": "kegg_align_dir", "type": "infile", "format": "align.blast.blast_xml_dir"},
-            {"name": "string_align_dir", "type": "infile", "format": "align.blast.blast_xml_dir"},
-            {"name": "gene_file", "type": "infile", "format": "rna.gene_list"},
-            {"name": "upper_limit", "type": "int", "default": 10},  # xml被拆为10份
-            {"name": "out_cog", "type": "outfile", "format": "annotation.cog.cog_table"},
-            {"name": "out_kegg", "type": "outfile", "format": "annotation.upload.anno_upload"},
-            {"name": "out_go", "type": "outfile", "format": "annotation.upload.anno_upload"}
+            {"name": "in_cog", "type": "infile", "format": "annotation.cog.cog_table"},
+            {"name": "in_kegg", "type": "infile", "format": "annotation.upload.anno_upload"},
+            {"name": "in_go", "type": "infile", "format": "annotation.upload.anno_upload"},
+            {"name": "database", "type": "string", "default": "go,kegg,string"}
         ]
         self.add_option(options)
-        self.xml_list = []
-        self.change_list = []
-        self.anno_list = []
+        self.kegg = self.add_tool("annotation.kegg_upload")
+        self.cog = self.add_tool("annotation.cog_upload")
+        self.go = self.add_tool("annotation.go_upload")
 
     def check_options(self):
         """
@@ -37,46 +33,26 @@ class AnnoUploadModule(Module):
 
     def run(self):
         super(AnnoUploadModule, self).run()
-        self.get_list()
-        # if self.option("blast_method") == "diamond":
-        self.run_anno()
+        self.on_rely([self.go, self.cog, self.kegg], self.end)
+        self.run_table()
 
-    def get_list(self):
-        nr_tmp = self.option("nr_align_dir").prop["path"]
-        kegg_tmp = self.option("kegg_align_dir").prop["path"]
-        string_tmp = self.option("string_align_dir").prop["path"]
-        for i in range(self.option("upper_limint")):
-            mark = "_" + str(i + 1) + "_"
-            file_tmp = []
-            for file in os.listdir(nr_tmp):
-                if file.find(mark) != -1 and not file.endswith("_new"):
-                    file = os.path.join(nr_tmp, file)
-                    file_tmp.append(file)
-            for file in os.listdir(string_tmp):
-                if file.find(mark) != -1 and not file.endswith("_new"):
-                    file = os.path.join(string_tmp, file)
-                    file_tmp.append(file)
-            for file in os.listdir(kegg_tmp):
-                if file.find(mark) != -1 and not file.endswith("_new"):
-                    file = os.path.join(kegg_tmp, file)
-                    file_tmp.append(file)
-            self.xml_list.append(file_tmp)
-
-    def run_anno(self):
-        for i in range(self.option("upper_limit")):
-            lst = self.xml_list[i]
-            opts = {
-                "blast_nr_xml": lst[0],
-                "blast_kegg_xml": lst[1],
-                "blast_string_xml": lst[2],
-                "gene_file": self.option("gene_file"),
-                'go_annot': True,
-                'nr_annot': True
+    def run_table(self):
+        if "go" in self.option("database"):
+            opts_go = {
+                "gos_list_upload": self.option("in_go")
             }
-            anno_module = self.add_module("annotation.ref_annotation")
-            anno_module.set_options(opts)
-            self.anno_list.append(anno_module)
-        if len(self.anno_list) != 1:
-            self.on_rely(self.anno_list, self.end)
-        else:
-            self.anno_list[0].on("end", self.end)
+            self.go.set_options(opts_go)
+            self.go.run()
+        if "kegg" in self.option("database"):
+            opts_kegg = {
+                "kos_list_upload": self.option("in_kegg")
+            }
+            self.kegg.set_options(opts_kegg)
+            self.kegg.run()
+        if "string" in self.option("database"):
+            opts_cog = {
+                "cogs_list_upload": self.option("in_cog")
+            }
+            self.cog.set_options(opts_cog)
+            self.cog.run()
+
