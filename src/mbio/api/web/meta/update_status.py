70488cf334827fc893813a375282f5503222becc
xuanhongdong <hongdong.xuan@majorbio.com>
2017-05-09 17:47:09 +0800

    一键化不更新状态表

--- src/mbio/api/web/meta/update_status.py
+++ src/mbio/api/web/meta/update_status.py
@@ -141,51 +141,54 @@ class UpdateStatus(Log):
                 collection.find_one_and_update({"_id": obj_id}, {'$set': data}, upsert=True)
             sg_status_col = self.mongodb['sg_status']
             if status == "start":
-                tmp_col = self.mongodb[collection_name]
-                try:
-                    temp_find = tmp_col.find_one({"_id": obj_id})
-                    tb_name = temp_find["name"]
-                    temp_params = temp_find['params']
-                    submit_location = json.loads(temp_params)['submit_location']
-                except:
-                    tb_name = ""
-                    temp_params = ''
-                    submit_location = ''
-                tmp_task_id = list()
-                print 'update_status task_id:', self.task_id
-                tmp_task_id = re.split("_", self.task_id)
-                tmp_task_id.pop()
-                tmp_task_id.pop()
-                insert_data = {
-                    "table_id": obj_id,
-                    "table_name": tb_name,
-                    "task_id": "_".join(tmp_task_id),
-                    "type_name": collection_name,
-                    "params": temp_params,
-                    "submit_location": submit_location,
-                    "status": "start",
-                    "is_new": "new",
-                    "desc": desc,
-                    "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                }
-                sg_status_col.insert_one(insert_data)
+                if not batch_id:
+                    tmp_col = self.mongodb[collection_name]
+                    try:
+                        temp_find = tmp_col.find_one({"_id": obj_id})
+                        tb_name = temp_find["name"]
+                        temp_params = temp_find['params']
+                        submit_location = json.loads(temp_params)['submit_location']
+                    except:
+                        tb_name = ""
+                        temp_params = ''
+                        submit_location = ''
+                    tmp_task_id = list()
+                    print 'update_status task_id:', self.task_id
+                    tmp_task_id = re.split("_", self.task_id)
+                    tmp_task_id.pop()
+                    tmp_task_id.pop()
+                    insert_data = {
+                        "table_id": obj_id,
+                        "table_name": tb_name,
+                        "task_id": "_".join(tmp_task_id),
+                        "type_name": collection_name,
+                        "params": temp_params,
+                        "submit_location": submit_location,
+                        "status": "start",
+                        "is_new": "new",
+                        "desc": desc,
+                        "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    }
+                    sg_status_col.insert_one(insert_data)
             elif status == "finish":  # 只能有一次finish状态
-                insert_data = {
-                    "status": 'end',
-                    "desc": desc,
-                    "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                }
-                sg_status_col.find_one_and_update({"table_id": obj_id, "type_name": collection_name},
-                                                  {'$set': insert_data}, upsert=True)
+                if not batch_id:
+                    insert_data = {
+                        "status": 'end',
+                        "desc": desc,
+                        "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    }
+                    sg_status_col.find_one_and_update({"table_id": obj_id, "type_name": collection_name},
+                                                      {'$set': insert_data}, upsert=True)
                 self.pipe_update(batch_id, collection_name, obj_id, "end", desc)
             else:
-                insert_data = {
-                    "status": status,
-                    "desc": desc,
-                    "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                }
-                sg_status_col.find_one_and_update({"table_id": obj_id, "type_name": collection_name},
-                                                  {'$set': insert_data}, upsert=True)
+                if not batch_id:
+                    insert_data = {
+                        "status": status,
+                        "desc": desc,
+                        "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    }
+                    sg_status_col.find_one_and_update({"table_id": obj_id, "type_name": collection_name},
+                                                      {'$set': insert_data}, upsert=True)
                 self.pipe_update(batch_id, collection_name, obj_id, status, desc)
             self._mongo_client.close()
 
