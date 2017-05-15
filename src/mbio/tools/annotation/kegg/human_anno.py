08b1ecaf33e39896dd33b892c7dffbbef8e68bcc
hesheng <sheng.he@majorbio.com>
2017-05-15 09:05:25 +0800

    修改

--- src/mbio/tools/annotation/kegg/human_anno.py
+++ src/mbio/tools/annotation/kegg/human_anno.py
@@ -3,6 +3,7 @@
 from biocluster.agent import Agent
 from biocluster.tool import Tool
 from biocluster.core.exceptions import OptionError
+import os
 
 
 class HumanAnnoAgent(Agent):
@@ -41,6 +42,7 @@ class HumanAnnoTool(Tool):
     def __init__(self, config):
         super(HumanAnnoTool, self).__init__(config)
         self._version = "1.0"
+        self.humann_scon = self.config.SOFTWARE_DIR + "/bioinfo/annotation/humann-0.99/SConstruct"
 
 
     def run(self):
@@ -55,12 +57,23 @@ class HumanAnnoTool(Tool):
         else:
             table_file = self.option('blastout').path
         try:
-            self.humann_anno()
+            self.humann_anno(table_file)
             self.end()
         except Exception:
             import traceback
             self.logger.debug(traceback.format_exc())
             self.set_error('humann注释出错！')
 
-    def humann_anno(self):
+    def humann_anno(self, blast_table):
+        if not os.path.exists("input"):
+            os.makedirs("input")
+        os.link(blast_table, "input")
+        with open(self.humann_scon) as f, open("SConstruct", 'w') as w:
+            w.write(f.read())
+        cmd = self.config.SOFTWARE_DIR + "/program/Python/bin/scons " + "--site-dir=" + os.path.dirname(self.humann_scon) + \
+        "site_scons " + "-C " + self.work_dir + " -j 2"
+        command = self.add_command("humann", cmd)
+        command.run()
+        self.wait()
+
         pass
