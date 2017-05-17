34d4387aad0ddff3e14281a7ca7b783c29e94280
qindanhua <qdh504f@126.com>
2017-05-17 09:43:57 +0800

    to file

--- webroot/mainapp/controllers/instant/ref_rna/geneset_class.py
+++ webroot/mainapp/controllers/instant/ref_rna/geneset_class.py
@@ -50,8 +50,8 @@ class GenesetClassAction(RefRnaController):
         elif data.anno_type == "kegg":
             table_name = "Kegg"
             collection_name = "sg_geneset_kegg_class"
-            to_file = 'ref_rna.export_multi_gene_list(geneset_kegg)'
-            option = {"geneset_kegg": data.geneset_id}
+            to_file = ['ref_rna.export_multi_gene_list(geneset_kegg)', "ref_rna.export_kegg_table(kegg_table)'"]
+            option = {"geneset_kegg": data.geneset_id, "kegg_table": data.geneset_id.split(",")[0]}
         else:
             info = {'success': False, 'info': '不支持的功能分类!'}
             return json.dumps(info)
