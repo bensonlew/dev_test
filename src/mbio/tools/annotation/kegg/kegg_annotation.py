c7e178ae7498e280d441ae5d6c8e2528bf47cd7e
zengjing <jing.zeng@majorbio.local>
2017-05-19 16:27:18 +0800

    图片转换软件路径更改

--- src/mbio/tools/annotation/kegg/kegg_annotation.py
+++ src/mbio/tools/annotation/kegg/kegg_annotation.py
@@ -67,7 +67,7 @@ class KeggAnnotationTool(Tool):
         super(KeggAnnotationTool, self).__init__(config)
         self._version = "2.0"
         self.taxonomy_path = self.config.SOFTWARE_DIR + "/database/KEGG/species/{}.ko.txt".format(self.option("taxonomy"))
-        self.image_magick = self.config.SOFTWARE_DIR + "//program/ImageMagick/bin/convert"
+        self.image_magick = self.config.SOFTWARE_DIR + "/program/ImageMagick/bin/convert"
 
     def run(self):
         super(KeggAnnotationTool, self).run()
