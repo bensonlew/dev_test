fe1a7796644990ac618a941a83e0c3ced56f1045
xuanhongdong <hongdong.xuan@majorbio.com>
2017-05-19 16:20:46 +0800

    修改nipt导表

--- src/mbio/api/database/nipt_analysis.py
+++ src/mbio/api/database/nipt_analysis.py
@@ -12,7 +12,9 @@ class NiptAnalysis(Base):
     def __init__(self, bind_object):
         super(NiptAnalysis, self).__init__(bind_object)
         self.mongo_client = Config().mongo_client
-        self.database = self.mongo_client['tsanger_nipt']
+        self.database = self.mongo_client['tsanger_nipt']  # 结果库
+        self.mongo_client_ref = Config().biodb_mongo_client
+        self.database_ref = self.mongo_client_ref['sanger_nipt_ref']  # 参考库
 
     # @report_check
     def add_zz_result(self, file_path, table_id=None, major=False):
@@ -63,3 +65,27 @@ class NiptAnalysis(Base):
         else:
             self.bind_object.logger.info("导入%s信息成功!" % file_path)
         return data_list
+
+
+    def add_bed_file(self, file_path):
+        """
+        用于添加nipt分析shell部分生成的bed文件到mongo表中
+        """
+        data_list = []
+        with open(file_path, 'rb') as r:
+            data1 = r.readlines()
+            for line in data1:
+                temp = line.rstrip().split("\t")
+                data = [("chr", int(temp[0])),
+                        ("start", int(temp[1])), ("end", int(temp[2])), ("gc", eval(temp[3])), ("map", eval(temp[4])),
+                        ("pn", eval(temp[5])), ("reads", int(temp[6])), ("sample_id", str(temp[7]))]
+                data_son = SON(data)
+                data_list.append(data_son)
+        try:
+            collection = self.database_ref["sg_nipt_bed"]
+            collection.insert_many(data_list)
+        except Exception, e:
+            raise Exception("导入%s信息出错:%s" % (file_path, e))
+        else:
+            self.bind_object.logger.info("导入%s信息到sg_nipt_bed成功!" % file_path)
+        return True
