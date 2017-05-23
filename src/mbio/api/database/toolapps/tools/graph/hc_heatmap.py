9d7ef3f73d37211e4694879cb44f1f3cf549fd29
zhouxuan <xuan.zhou@majorbio.com>
2017-05-23 12:56:42 +0800

    修改字段名称

--- src/mbio/api/database/toolapps/tools/graph/hc_heatmap.py
+++ src/mbio/api/database/toolapps/tools/graph/hc_heatmap.py
@@ -34,16 +34,16 @@ class HcHeatmap(Base):
         导入heatmap图相关信息
         """
         with open(self.output_dir + '/result_data') as f:
-            self.bind_object.logger.info("hc_heatmap主表写入")
-            hc_heatmap_id = self.db['hc_heatmap'].insert_one(SON(
+            self.bind_object.logger.info("heatmap主表写入")
+            heatmap_id = self.db['heatmap'].insert_one(SON(
                 project_sn=self.bind_object.sheet.project_sn,
                 task_id=self.bind_object.id,
-                name='hc_heatmap',
+                name='heatmap',
                 desc='聚类热图',
                 status='failed',
                 created_ts=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             )).inserted_id
-            self.bind_object.logger.info("hc_heatmap主表写入结束")
+            self.bind_object.logger.info("heatmap主表写入结束")
             insert_data = []
             head = f.next().strip('\r\n')  # windows换行符
             head = re.split('\t', head)
@@ -53,18 +53,18 @@ class HcHeatmap(Base):
                 line = re.split('\t', line)
                 sample_num = line[1:]
                 otu_detail = dict()
-                otu_detail['hc_heatmap_id'] = hc_heatmap_id
+                otu_detail['heatmap_id'] = heatmap_id
                 otu_detail['row_name'] = line[0]
                 for i in range(0, len(sample_num)):
                     otu_detail[new_head[i]] = sample_num[i]
                 insert_data.append(otu_detail)
-            self.bind_object.logger.info("hc_heatmap表格数据导入开始")
+            self.bind_object.logger.info("heatmap表格数据导入开始")
             try:
-                self.db['hc_heatmap_detail'].insert_many(insert_data)
+                self.db['heatmap_detail'].insert_many(insert_data)
             except Exception as e:
-                self.bind_object.logger.info("hc_heatmap数据表导入出错{}".format(e))
+                self.bind_object.logger.info("heatmap数据表导入出错{}".format(e))
             else:
-                self.bind_object.logger.info("hc_heatmap数据表导入完成")
+                self.bind_object.logger.info("heatmap数据表导入完成")
             row_tree_path = self.output_dir + '/row_tre'
             if os.path.exists(row_tree_path):
                 self.bind_object.logger.info("拥有行聚类树")
@@ -85,19 +85,19 @@ class HcHeatmap(Base):
             else:
                 col_tree = ""
                 col_list = []
-            self.bind_object.logger.info("hc_heatmap_id：{}".format(hc_heatmap_id))
+            self.bind_object.logger.info("heatmap_id：{}".format(heatmap_id))
             try:
-                self.db['hc_heatmap'].update_one({'_id': hc_heatmap_id}, {'$set':
+                self.db['heatmap'].update_one({'_id': heatmap_id}, {'$set':
                                                             {'status': 'end',
                                                             'row_tree': row_tree,
                                                             'row_list': row_list,
                                                             'col_tree': col_tree,
                                                             'col_list': col_list}})
             except Exception as e:
-                self.bind_object.logger.info("hc_heatmap主表更新出错{}".format(e))
+                self.bind_object.logger.info("heatmap主表更新出错{}".format(e))
             else:
-                self.bind_object.logger.info("hc_heatmap主表更新完成")
-            return hc_heatmap_id
+                self.bind_object.logger.info("heatmap主表更新完成")
+            return heatmap_id
 
     def check(self):
         """
