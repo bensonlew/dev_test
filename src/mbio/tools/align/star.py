d163b993c2b4bee57b6d18e03137c3679a3f278d
zengjing <jing.zeng@majorbio.local>
2017-06-05 12:32:09 +0800

    返回码修改

--- src/mbio/tools/align/star.py
+++ src/mbio/tools/align/star.py
@@ -8,7 +8,7 @@ from biocluster.tool import Tool
 import shutil
 import os
 import glob
-from biocluster.core.exceptions import OptionError 
+from biocluster.core.exceptions import OptionError
 import json
 import time
 
@@ -21,7 +21,7 @@ class StarAgent(Agent):
     def __init__(self, parent):
         super(StarAgent, self).__init__(parent)
         options = [
-        
+
             {"name": "ref_genome_custom", "type": "infile", "format": "sequence.fasta"},  # 用户上传参考基因组文件
             {"name": "ref_genome", "type": "string"},  # 参考基因组模式选项 用户自定义、选择已有生物物种
             {"name": "ref_gtf", "type": "infile", "format": "gene_structure.gtf"},  # 参考基因组的gtf文件 ，gtf文件和fasta文件要配套使用
@@ -31,13 +31,13 @@ class StarAgent(Agent):
             {"name": "seq_method", "type": "string"},
             {"name": "is_indexed", "type": "bool", "default": False},
             {"name": "sample", "type": "string", "default": ""}
-           
+
         ]
         self.add_option(options)
         self.step.add_steps('star')
         self.on('start', self.step_start)
         self.on('end', self.step_end)
-        
+
     def step_start(self):
         self.step.star.start()
         self.step.update()
@@ -86,17 +86,17 @@ class StarTool(Tool):
         if not os.path.exists("ref_star_index2"):   # 创建第二次建索引的目录文件夹
             os.mkdir("ref_star_index2")
         self.genomeDir_path2 = os.path.join(self.work_dir, "ref_star_index2")
-           
+
     def star_index1(self, genomeDir, ref_fa):
         """
         step1:第一步建索引；用star建立参考基因组的索引，当用户不上传参考基因组时，该步骤省略，直接调用已有的序列文件
         """
         cmd = "{}STAR --runMode genomeGenerate --limitGenomeGenerateRAM 50000000000 --genomeDir {} --genomeFastaFiles {} --runThreadN 20".format(self.star_path, genomeDir, ref_fa)  # self.work_dir/ref_star_index1 用于存放第一步建立的参考基因组索引的路径,参数为用户上传的参考基因组文件
-        print cmd 
+        print cmd
         self.logger.info("使用star建立参考基因组索引")
         command = self.add_command("star_index1", cmd)
         command.run()
-        self.wait()
+        self.wait(command)
         if command.return_code == 0:
             self.logger.info("成功构建参考序列索引index1！")
         else:
@@ -112,7 +112,7 @@ class StarTool(Tool):
         """
         step2:第二步比对；用star进行单端序列的比对
         """
-       
+
         cmd = "{}STAR --runThreadN 20 --genomeDir {} --readFilesIn {}".format(self.star_path, genomeDir, self.option("readFilesIN").prop["path"])
         print cmd
         self.logger.info("使用STAR对序列进行单端mapping")
@@ -124,12 +124,12 @@ class StarTool(Tool):
         else:
             self.set_error("单端比对第一步比对出错!")
             raise Exception("运行star出错")
-            
+
     def star_aln1_pe(self, genomeDir):
         """
         step2:第二步比对；用star进行双端序列的比对
         """
-       
+
         cmd = "{}STAR --runThreadN 20 --genomeDir {} --readFilesIn {} {}".format(self.star_path, genomeDir, self.option("readFilesIN1").prop["path"], self.option("readFilesIN2").prop["path"])
         print cmd
         self.logger.info("使用STAR对序列进行双端mapping")
@@ -141,7 +141,7 @@ class StarTool(Tool):
         else:
             self.set_error("双端比对第一步比对出错!")
             raise Exception("运行star出错")
-            
+
     def star_index2(self, ref_fa, sj):
         """
         step3：第三步，第二次建索引，用于最终比对
@@ -163,7 +163,7 @@ class StarTool(Tool):
             else:
                 self.set_error("第二次索引建立出错!")
                 raise Exception("运行star出错")
-            
+
     def star_aln2_se(self):
         """
         step4：第四步，最终比对
@@ -174,7 +174,7 @@ class StarTool(Tool):
         self.logger.info("使用star进行最终比对！")
         command = self.add_command("star_aln2", cmd)
         command.run()
-        self.wait()
+        self.wait(command)
         if command.return_code == 0:
             self.logger.info("最终单端比对成功！")
         else:
@@ -185,7 +185,7 @@ class StarTool(Tool):
             else:
                 self.set_error("最终单端比对出错!")
                 raise Exception("运行star出错")
-         
+
     def star_aln2_pe(self):
         """
         step4：第四步，最终比对
@@ -215,7 +215,7 @@ class StarTool(Tool):
         self.logger.info("最终比对过程")
         command = self.add_command("star_aln2_bam", cmd)
         command.run()
-        self.wait()
+        self.wait(command)
         if command.return_code == 0:
             self.logger.info("最终双端比对成功！")
         else:
@@ -248,16 +248,16 @@ class StarTool(Tool):
             else:
                 self.set_error("最终单端比对出错!")
                 raise Exception("运行star出错")
-            
+
     def run(self):
         """
         运行
         """
         super(StarTool, self).run()
-        
+
         if self.option("ref_genome") == "customer_mode" and self.option("ref_genome_custom").is_set:  # 自定义模式的时候需要建索引
             self.logger.info("在参考基因组为自定义模式下运行star！")
-                    
+
             ref_fa = self.option("ref_genome_custom").prop["path"]
             if not os.path.exists("ref_star_index1"):
                 os.mkdir("ref_star_index1")
@@ -271,7 +271,7 @@ class StarTool(Tool):
                 self.star_aln2_pe()
                 self.star_aln2_pe_bam()
                 # time.sleep(30)
-                
+
             else:
                 self.star_aln1_se(genomeDir_path1)
                 sj = os.path.join(self.work_dir, "SJ.out.tab")
