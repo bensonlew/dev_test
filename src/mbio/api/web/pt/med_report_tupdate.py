47c202a17717fe3610d2157f1b83a87502fa751d
moli.zhou <moli.zhou@majorbio.com>
2017-06-08 16:56:38 +0800

    修改URL

--- src/mbio/api/web/pt/med_report_tupdate.py
+++ src/mbio/api/web/pt/med_report_tupdate.py
@@ -6,16 +6,17 @@ from bson.objectid import ObjectId
 from biocluster.config import Config
 from biocluster.core.function import filter_error_info
 from mbio.api.web.meta.update_status import UpdateStatus
+from biocluster.wpm.log import Log
 
 
-class MedReportTupdate(UpdateStatus):
+class MedReportTupdate(Log):
 
     def __init__(self, data):
-        super(UpdateStatus, self).__init__(data)
+        super(MedReportTupdate, self).__init__(data)
         self._config = Config()
         self._client = "client03"
         self._key = "hM4uZcGs9d"
-        self._url = "http://www.tsanger.com/api/add_file"
+        self._url = "http://api.tsanger.com/task/add_file"
         self._mongo_client = self._config.mongo_client
         self.database = self._mongo_client[Config().MONGODB+'_paternity_test']
 
