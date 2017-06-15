77c5edced3dd036b741d17470505ffe3ec2f00e2
xuanhongdong <hongdong.xuan@majorbio.com>
2017-06-15 09:59:36 +0800

    修改接口basic

--- webroot/mainapp/controllers/core/basic.py
+++ webroot/mainapp/controllers/core/basic.py
@@ -5,6 +5,7 @@ import traceback
 import time
 import json
 from mainapp.models.mongo.meta import Meta
+from mainapp.models.mongo.test import Test
 from biocluster.wpm.client import worker_client, wait
 
 
@@ -14,6 +15,7 @@ class Basic(object):
         self._json = data
         self._id = data["id"]
         self._return_msg = None
+        # self.data = Meta()
 
     @property
     def id(self):
@@ -53,6 +55,21 @@ class Basic(object):
             update_info = None
         if "main_table_data" in self._json['options'].keys():
             del self._json['options']['main_table_data']
+
+        # worker = worker_client()
+        # info = worker.add_task(self._json)
+        # if "success" in info.keys() and info["success"]:
+        #     if update_info and main_table_data:
+        #         for i in update_info:
+        #             if i == "batch_id":
+        #                 continue
+        #             Meta().insert_main_table_new(update_info[i], i, main_table_data)
+        #         if self.instant:
+        #             return self.instant_wait(worker)
+        #         else:
+        #             return info
+        # else:
+        #     raise Exception("任务提交失败:%s" % info["info"])
         try:
             worker = worker_client()
             info = worker.add_task(self._json)
@@ -74,34 +91,6 @@ class Basic(object):
             print "ERROR:", exstr
             raise Exception("任务提交失败：%s, %s" % (str(e), str(exstr)))
 
-            # raise Exception("任务提交失败：%s" % (str(e)))
-            # try:
-            #     worker = worker_client()
-            #     info = worker.add_task(self._json)
-            #     if "success" in info.keys() and info["success"]:
-            #         for obj_id, collection_name in update_info.items():
-            #             Meta().insert_main_table_new(collection_name, obj_id, main_table_data)
-            #         if self.instant:
-            #             return self.instant_wait(worker)
-            #         else:
-            #             return info
-            #     else:
-            #         return {"success": False, "info": "%s"%(info["info"])}
-            # except Exception as e:
-                # try:
-                #     worker = worker_client()
-                #     info = worker.add_task(self._json)
-                #     if "success" in info.keys() and info["success"]:
-                #         if self.instant:
-                #             return self.instant_wait(worker)
-                #         else:
-                #             return info
-                #     else:
-                #         return {"success": False, "info": "%s"%(info["info"])}
-            #     except Exception, e:
-            #     #     exstr = traceback.format_exc()
-            #         raise Exception("任务提交失败：%s" % (str(e)))
-
     def instant_wait(self, worker):
         end = wait(self._id)
         if end is True:
@@ -109,3 +98,6 @@ class Basic(object):
             return self._return_msg
         else:
             raise Exception("运行超时!")
+
+    # def insert_main_table_new(self, collection, obj_id, data):
+    #     return self.db[collection].find_one_and_update({"_id": ObjectId(obj_id)}, {'$set': data}, upsert=True)
