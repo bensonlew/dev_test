8661e236eb0ef9abdb3f2a2d7002a0eec0750e71
xuanhongdong <hongdong.xuan@majorbio.com>
2017-06-24 13:57:16 +0800

    修改状态更新

--- src/mbio/api/web/meta/update_status.py
+++ src/mbio/api/web/meta/update_status.py
@@ -45,26 +45,14 @@ class UpdateStatus(Log):
             content['files'] = self.data["sync_task_log"]["files"]
         if 'dirs' in self.data['sync_task_log'].keys():
             content['dirs'] = self.data['sync_task_log']['dirs']
+        # if 'base_path' in self.data['sync_task_log'].keys():
+        #     content['base_path'] = self.data['sync_task_log']['base_path']
         data['sync_task_log'] = json.dumps(content, cls=CJsonEncoder)
         return urllib.urlencode(data)
 
-    def update(self, web_api=True):
-        self.logger.info("开始执行update_status")
-        try:
-            self.update_status()
-        except Exception, e:
-            exstr = traceback.format_exc()
-            print exstr
-            print e
-            sys.stdout.flush()
-            self.logger.info("执行update_status出错")
-        self.logger.info("执行update_status结束")
-        if web_api and 'files' in self.data['sync_task_log'].keys() or 'dirs' in self.data['sync_task_log'].keys():
-            self.logger.info("执行post")
-            super(UpdateStatus, self).update(save=False)
-            self.logger.info("执行post结束")
-        self.set_end()
-        self.model.save()
+    def update(self):
+        if 'files' in self.data['sync_task_log'].keys() or 'dirs' in self.data['sync_task_log'].keys():
+            self.send()
 
     def update_status(self):
         status = self.data["sync_task_log"]["task"]["status"]
@@ -169,7 +157,9 @@ class UpdateStatus(Log):
             self.logger.info("查询end_counts个数:{}".format(end_counts))  # 测试完成后删除
             # 多样性指数失败，则T检验失败，但是t检验失败后没有插表，这里通过判断多样性指数失败个数来间接判断t检验
             diversity_end_counts = self.mongodb['sg_pipe_detail'].find({'pipe_batch_id': batch_id,
-                                                                        "submit_location": "alpha_diversity_index",
+                                                                        "submit_location":
+                                                                            {'$in': ["alpha_diversity_index",
+                                                                                     "otu_pan_core"]},
                                                                         'status': "failed"}).count()
             self.logger.info("查询diversity_end_counts个数:{}".format(diversity_end_counts))  # 测试完成后删除
             update_counts = end_counts + diversity_end_counts
