764e735eb1389245022145ff83416927a4ff226f
孔华磊 <hualei.kong@majorbio.com>
2017-07-05 20:00:37 +0800

    Update diff_exp.py 修改参数diff_ci diff_fdr_ci参数设置

--- src/mbio/tools/rna/diff_exp.py
+++ src/mbio/tools/rna/diff_exp.py
@@ -68,10 +68,10 @@ class DiffExpAgent(Agent):
             raise OptionError("必须设置输入文件:基因表达量表")
         if not self.option('control_file').is_set:
             raise OptionError("必须设置输入文件：上下调对照组参考文件")
-        if self.option("diff_ci") > 1 or self.option("diff_ci") < 0:
-            raise OptionError("显著性水平不在(0,1)范围内")
-        if self.option("diff_fdr_ci") > 1 or self.option("diff_fdr_ci") < 0:
-            raise OptionError("显著性水平不在(0,1)范围内")
+        if self.option("diff_ci") >= 1 or self.option("diff_ci") < 0:
+            raise OptionError("显著性水平不在[0,1)范围内")
+        if self.option("diff_fdr_ci") >= 1 or self.option("diff_fdr_ci") < 0:
+            raise OptionError("显著性水平不在[0,1)范围内")
         if self.option("fc") < 0:
             raise OptionError("显著性水平不能负数")
         if self.option("diff_rate") > 1 or self.option("diff_rate") <= 0:
