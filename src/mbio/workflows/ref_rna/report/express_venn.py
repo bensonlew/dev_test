bbb5fbe670c02409e57c1647c3264ba53103f115
khl0798 <khl0798@163.com>
2017-07-12 12:51:15 +0800

    express_venn workflow

--- src/mbio/workflows/ref_rna/report/express_venn.py
+++ src/mbio/workflows/ref_rna/report/express_venn.py
@@ -40,18 +40,13 @@ class ExpressVennWorkflow(Workflow):
         
     def run_venn(self, fpkm_path, specimen):
         """样本间特异性基因venn图"""
-        if self.group_id in ['all', 'All', 'ALL']:
-            new_fpkm = fpkm_path
-        else:
-            new_fpkm = self.get_sample_table(fpkm_path,specimen)
         options = {
-            "express_matrix": new_fpkm,
+            "express_matrix": fpkm_path,
             "group_table": self.option("group_id"),
             "threshold":self.option("threshold")
         }
         
         self.logger.info("检查new_fpkm和new_group的路径:")
-        self.logger.info(new_fpkm)
         self.venn.set_options(options)
         self.venn.on('end', self.set_db)
         self.venn.run()
@@ -79,40 +74,14 @@ class ExpressVennWorkflow(Workflow):
                 self.samples.append(line[0])
         print self.samples
         return self.samples
-        
-    def get_sample_table(self,fpkm_path, specimen):
-        """ 根据筛选的样本名生成新的fpkm表 和 group_table表 """
-        fpkm = pd.read_table(fpkm_path,sep="\t",)
-        sample_name = fpkm.columns[1:]
-        del_sam = []
-        for sam in sample_name:
-            try:
-                if sam not in specimen:
-                    del_sam.append(sam)
-            except Exception:
-                pass
-        # if self.option("threshold"):
-        #     """过滤"""
-        #     threshold = self.option("threshold")
-        #     for i in sample_name:
-        #         fpkm[i]=fpkm[i].apply(lambda x: x if(x>=threshold)else 0)
-        if del_sam:
-            new_fpkm = fpkm.drop(del_sam, axis=1)
-            self.new_fpkm = self.venn.work_dir + "/fpkm"
-            header=['']
-            header.extend(specimen)
-            new_fpkm.columns = header
-            new_fpkm.to_csv(self.new_fpkm, sep="\t",index=False)
-            print 'end!'
-            return self.new_fpkm
-        else:
-            self.new_fpkm = self.venn.work_dir + "/fpkm"
-            fpkm.to_csv(self.new_fpkm,sep="\t",index=False)
-            return self.new_fpkm
-        
+
     def run(self):
         fpkm = self.option("express_file").split(",")[0]
-        specimen = self.get_samples()
+        if self.group_id in ['all','All','ALL']:
+            with open(fpkm,'r+') as f1:
+                specimen = f1.readline().strip().split("\t")
+        else:
+            specimen = self.get_samples()
         self.run_venn(fpkm, specimen)
         super(ExpressVennWorkflow, self).run()
     
