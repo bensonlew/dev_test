5317aeefac6d3c0c4832e873a7aa0f63a0e05bdd
deqing.gu <deqing.gu@majorbio.com>
2017-07-27 17:44:52 +0800

    增加参数判断，不能同时选择聚类方式为无；规范代码格式

--- webroot/mainapp/controllers/submit/ref_rna/geneset_cluster.py
+++ webroot/mainapp/controllers/submit/ref_rna/geneset_cluster.py
@@ -21,12 +21,43 @@ from mbio.api.to_file.ref_rna import *
 from bson import ObjectId
 from mainapp.models.mongo.ref_rna import *
 
+
 class GenesetClusterAction(RefRnaController):
     def __init__(self):
         super(GenesetClusterAction, self).__init__(instant=False)
     
     def GET(self):
         return 'khl'
+
+    @staticmethod
+    def check_options(data, method):
+        """
+        检查网页端传来的参数是否正确
+        """
+        if method == 'hclust':
+            params_name = ['type', 'genes_distance_method', 'method', 'log', 'sub_num', "level",
+                           'samples_distance_method', 'group_id', 'group_detail', 'geneset_id',
+                           'express_method', 'submit_location', 'genes_distance_algorithm',
+                           'samples_distance_algorithm']
+        elif method == 'kmeans':
+            params_name = ['type', 'genes_distance_method', 'method', 'log', 'sub_num', "level",
+                           'group_id', 'group_detail', 'geneset_id',
+                           'express_method', 'submit_location']
+        else:
+            params_name = []
+        success = list()
+
+        for names in params_name:
+            if not hasattr(data, names):
+                success.append("缺少参数: {}".format(names))
+        if method == 'hclust':
+            if data.samples_distance_method == "" and data.genes_distance_method == "":
+                success.append("不能同时选择‘无’ ！")
+
+        geneset_id = str(data.geneset_id)
+        if not isinstance(geneset_id, ObjectId) and not isinstance(geneset_id, types.StringType):
+            success.append("传入的geneset_id {}不是一个ObjectId对象或字符串类型".format(geneset_id))
+        return success
     
     def POST(self):
         data = web.input()
@@ -40,15 +71,15 @@ class GenesetClusterAction(RefRnaController):
             return json.dumps(info)
         my_param = dict()
         my_param['submit_location'] = data.submit_location
-        my_param['type'] = data.type # gene or transcript
+        my_param['type'] = data.type  # gene or transcript
         # my_param['distance_method']=data.distance_method # 距离算法
-        my_param['method'] = data.method #聚类方法 kmeans or hclust
+        my_param['method'] = data.method  # 聚类方法 kmeans or hclust
         my_param['log'] = data.log
         my_param['level'] = data.level  # fpkm or tpm
         my_param['sub_num'] = data.sub_num
         my_param['group_id'] = data.group_id
         my_param['group_detail'] = group_detail_sort(data.group_detail)
-        my_param['express_method'] = data.express_method # featurecounts or rsem
+        my_param['express_method'] = data.express_method  # featurecounts or rsem
         my_param['geneset_id'] = data.geneset_id
         my_param["task_type"] = task_type
         my_param['genes_distance_method'] = data.genes_distance_method
@@ -98,12 +129,15 @@ class GenesetClusterAction(RefRnaController):
                 mongo_data.append(('sample_cluster', False))
             else:
                 mongo_data.append(("sample_cluster", True))
+
         collection_name = "sg_geneset_cluster"
         express_id = self.ref_rna.get_express_id(task_info['task_id'], data.level, data.express_method)
         if not express_id:
-             info = {"success": False, "info": "sg_express表params参数中没有找到表达量水平type:{},表达量计算软件exp    ress_method{}的表达量信息,请换一种表达量或表达量值类型".format(data.type,data.express_method)}
-             return json.dumps(info)
-        #添加class_code信息
+            info = {"success": False, "info": "sg_express表params参数中没有找到表达量水平type:{},"
+                                              "表达量计算软件express_method{}的表达量信息。"
+                                              "请换一种表达量或表达量值类型".format(data.type, data.express_method)}
+            return json.dumps(info)
+        # 添加class_code信息
         try:
             class_code_id = self.ref_rna.get_class_code_id(geneset_info['task_id'])
             print 'class_code_id'
@@ -125,7 +159,7 @@ class GenesetClusterAction(RefRnaController):
             "group_detail": data.group_detail,
             "type": data.type,  # 对应gene or transcript
             "express_method": data.express_method,
-            "express_level": data.level, #对应fpkm or tpm
+            "express_level": data.level,  # 对应fpkm or tpm
             "sub_num": data.sub_num,
             "geneset_cluster_id": str(main_table_id),
             "gene_list": data.geneset_id,
@@ -137,40 +171,20 @@ class GenesetClusterAction(RefRnaController):
             options['genes_distance_algorithm'] = data.genes_distance_algorithm
             options['samples_distance_algorithm'] = data.samples_distance_algorithm
 
-        to_file = ["ref_rna.export_express_matrix_level(express_file)","ref_rna.export_group_table_by_detail(group_id)", "ref_rna.export_gene_list(gene_list)","ref_rna.export_class_code(class_code)"]
-        self.set_sheet_data(name=task_name, options=options, main_table_name=main_table_name,\
-                task_id = task_info['task_id'],project_sn = task_info['project_sn'],\
-                params = my_param, to_file = to_file)
+        to_file = ["ref_rna.export_express_matrix_level(express_file)",
+                   "ref_rna.export_group_table_by_detail(group_id)",
+                   "ref_rna.export_gene_list(gene_list)",
+                   "ref_rna.export_class_code(class_code)"]
+        self.set_sheet_data(name=task_name, options=options, main_table_name=main_table_name,
+                            task_id=task_info['task_id'], project_sn=task_info['project_sn'],
+                            params=my_param, to_file=to_file)
         
         task_info = super(GenesetClusterAction, self).POST()
         task_info['content'] = {'ids': {'id': str(main_table_id), 'name': main_table_name}}
         print task_info
         return json.dumps(task_info)
 
-    @staticmethod
-    def check_options(data, method):
-        """
-        检查网页端传来的参数是否正确
-        """
-        if method == 'hclust':
-            params_name = ['type', 'genes_distance_method', 'method', 'log', 'sub_num', "level",
-                           'samples_distance_method', 'group_id', 'group_detail', 'geneset_id',
-                           'express_method', 'submit_location', 'genes_distance_algorithm',
-                           'samples_distance_algorithm']
-        if method == 'kmeans':
-            params_name = ['type', 'genes_distance_method', 'method', 'log', 'sub_num', "level",
-                           'group_id', 'group_detail', 'geneset_id',
-                           'express_method', 'submit_location']
-        success = list()
-        
-        for names in params_name:
-            if not hasattr(data, names):
-                success.append("缺少参数！{}".format(names))
-        
-        geneset_id = str(data.geneset_id)
-        if not isinstance(geneset_id, ObjectId) and not isinstance(geneset_id, types.StringType):
-            success.append("传入的geneset_id {}不是一个ObjectId对象或字符串类型".format(geneset_id))
-        return success
+
         
         
             
