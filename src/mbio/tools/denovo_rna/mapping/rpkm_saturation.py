fd46c95d40ee5363af68c6046d238430392ceaac
caiping.shi <caiping.shi@majorbio.com>
2017-08-30 07:51:02 +0800

    将rpkm_saturation中内存“25G”改为“30G”

--- src/mbio/tools/denovo_rna/mapping/rpkm_saturation.py
+++ src/mbio/tools/denovo_rna/mapping/rpkm_saturation.py
@@ -54,7 +54,7 @@ class RpkmSaturationAgent(Agent):
         所需资源
         """
         self._cpu = 10
-        self._memory = '25G'
+        self._memory = '30G'
 
     def end(self):
         result_dir = self.add_upload_dir(self.output_dir)
