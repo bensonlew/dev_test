38ec463b1db17128dab766302da6545db9a7fa93
deqing.gu <deqing.gu@majorbio.com>
2017-09-01 13:17:09 +0800

    1. pvalue=0, --->pvalue=1e-320, pvalue=1e-1000,会导致log转换失败。 2. 修复对nosig，down，up的判断

--- src/mbio/api/database/refrna_express.py
+++ src/mbio/api/database/refrna_express.py
@@ -1028,14 +1028,14 @@ class RefrnaExpress(Base):
         # dump data of diff_stat_path into mongodb
         diff_table = pd.read_table(diff_stat_path)
         sig_status = list()
-        sig_mark = list(diff_table['significant'])
-        reg_list = list(diff_table['regulate'])
-        if 'no' in sig_mark:
+        sig_mark = diff_table['significant']
+        reg_list = diff_table['regulate']
+        if 'no' in list(sig_mark):
             sig_status.append('nosig')
-        if 'yes' in sig_mark:
-            if 'down' in reg_list:
+        if 'yes' in list(sig_mark):
+            if 'down' in list(reg_list[sig_mark == 'yes']):
                 sig_status.append('down')
-            if 'up' in reg_list:
+            if 'up' in list(reg_list[sig_mark == 'yes']):
                 sig_status.append('up')
         sig_pvalues = diff_table[pvalue_padjust][diff_table['significant'] == "yes"]
         log10_pvalue_list = sorted([-log10(x) for x in sig_pvalues if x > 0])
@@ -1051,7 +1051,7 @@ class RefrnaExpress(Base):
         elif len(sig_pvalues) == 0:
             tmp_list = sorted([-log10(x) for x in diff_table[pvalue_padjust] if x > 0])
             if len(tmp_list) == 0:
-                log10_pvalue_cutoff = 10000
+                log10_pvalue_cutoff = 320
             else:
                 log10_pvalue_cutoff = tmp_list[int(len(tmp_list)*0.9)]
         else:
@@ -1079,11 +1079,15 @@ class RefrnaExpress(Base):
 
             pvalue = row_dict[pvalue_padjust]
             if pvalue <= 0:
-                pvalue = 1e-10000
-            if -log10(pvalue) > log10_pvalue_cutoff:
-                log10_pvalue = log10_pvalue_cutoff
-            else:
-                log10_pvalue = -log10(pvalue)
+                pvalue = 1e-320
+            try:
+                if -log10(pvalue) > log10_pvalue_cutoff:
+                    log10_pvalue = log10_pvalue_cutoff
+                else:
+                    log10_pvalue = -log10(pvalue)
+            except:
+                print(type(pvalue))
+                raise Exception('pvalue is {}'.format(pvalue))
             row_dict['log10_'+pvalue_padjust] = log10_pvalue
 
             row_dict.update(marker_info)
