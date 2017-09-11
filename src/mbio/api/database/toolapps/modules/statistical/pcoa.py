65a9a1b69ca203b8149a8b6be28a76435be94c5d
zengjing <jing.zeng@majorbio.local>
2017-09-11 17:07:48 +0800

    pcoa修改

--- src/mbio/api/database/toolapps/modules/statistical/pcoa.py
+++ src/mbio/api/database/toolapps/modules/statistical/pcoa.py
@@ -25,25 +25,50 @@ class Pcoa(Base):
         """
         运行函数
         """
-        self.main_id = self.pcoa_in()
-        self.table_ids = self.table_in()
-        return self.main_id
-        pass
+        if self.bind_object._task.option("group_table").is_set:
+            group_detail = self.get_group_detail(self.bind_object._task.option("group_table").prop["path"])
+            self.bind_object.logger.info(group_detail)
+        else:
+            group_detail = None
+        self.main_id = self.pcoa_in(group_detail)
+        self.table_ids = self.table_in(group_detail)
 
-    def table_in(self):
+    def get_group_detail(self, group_table):
         """
-		导入表格相关信息
-		"""
+        根据分组文件得到具体的分组方案
+        """
+        group_samples = {}  # 分组对应中新样本对应的旧样本
+        with open(group_table, "r") as f:
+            line = f.readline().rstrip()
+            line = re.split("\t", line)
+            if line[1] == "##empty_group##":
+                is_empty = True
+            else:
+                is_empty = False
+            for i in range(1, len(line)):
+                group_samples[line[i]] = []
+            for item in f:
+                item = item.rstrip().split("\t")
+                for i in range(1, len(line)):
+                    if item[i]:
+                        group_samples[line[i]].append(item[0])
+                    else:
+                        self.bind_object.logger.info("{}样本不在分组方案{}内".format(item[0], line[i]))
+        return group_samples
 
+    def table_in(self, group_detail=None):
+        """
+        导入主表信息
+        """
         ratation1 = self.insert_table(self.output_dir + '/Pcoa/pcoa_sites.xls', '样本坐标表',
-                                     '样本坐标表')
+                                     '样本坐标表', group_detail)
         ratation2 = self.insert_table(self.output_dir + '/Pcoa/pcoa_eigenvalues.xls', '矩阵特征值表',
-                                      '矩阵特征值')
+                                      '矩阵特征值', group_detail)
         ratation3 = self.insert_table(self.output_dir + '/Pcoa/pcoa_eigenvaluespre.xls', '主成分解释度表',
-                                      '主成分解释度表')
+                                      '主成分解释度表', group_detail)
         return [ratation1, ratation2, ratation3]
 
-    def insert_table(self, fp, name, desc):
+    def insert_table(self, fp, name, desc, group_detail=None):
         self.bind_object.logger.info('开始导入{}表'.format(fp))
         with open(fp) as f:
             line = 'ID' + f.readline()
@@ -56,6 +81,7 @@ class Pcoa(Base):
                 attrs=columns,
                 desc=desc,
                 status='end',
+                group_detail=group_detail,
                 created_ts=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             )).inserted_id
             for line in f:
@@ -74,7 +100,7 @@ class Pcoa(Base):
                 self.insert_specimens(self.sample_list)
         return table_id
 
-    def pcoa_in(self):
+    def pcoa_in(self, group_detail=None):
         """
         导入pcoa相关信息
         """
@@ -85,6 +111,7 @@ class Pcoa(Base):
             name='pcoa',
             desc='pcoa分析',
             status='end',
+            group_detail=group_detail,
             created_ts=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         )).inserted_id
         self.bind_object.logger.info('主表导入结束')
@@ -117,7 +144,7 @@ class Pcoa(Base):
 
     def insert_specimens(self, specimen_names):
         """
-		"""
+        """
         task_id = self.bind_object.id
         project_sn = self.bind_object.sheet.project_sn
         datas = [SON(project_sn=project_sn, task_id=task_id, name=i) for i in specimen_names]
