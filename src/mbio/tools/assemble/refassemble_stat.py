cc86716b7446d69c8ff4e139df4bd217e707bf29
wangzhaoyue <zhaoyue.wang@majorbio.local>
2017-09-13 16:11:11 +0800

    组装统计change_id_merged.fa改为merged.fa

--- src/mbio/tools/assemble/refassemble_stat.py
+++ src/mbio/tools/assemble/refassemble_stat.py
@@ -135,13 +135,13 @@ class RefassembleStatTool(Tool):
         """
         all_file = os.listdir(self.option('all_files_dir'))
         for f in all_file:
-            if f.endswith("change_id_merged.fa"):
+            if f.endswith("merged.fa"):
                 files = os.path.join(self.option('all_files_dir'), f)
                 steps = [200, 300, 600, 1000]
                 for step in steps:
                     step_count(files, self.option('all_files_dir') + "/" + f + ".txt", 10, step,
                                self.work_dir + "/trans_count_stat_" + str(step) + ".txt")
-            if f.endswith("change_id_merged.gtf"):
+            if f.endswith("merged.gtf"):
                 files = os.path.join(self.option('all_files_dir'), f)
                 code_count = os.path.join(self.work_dir, "code_num.txt")
                 class_code_count(files, code_count)
