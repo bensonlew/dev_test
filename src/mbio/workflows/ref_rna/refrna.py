ea00e5348e91b0eaad5852f73e0cc08c1114a69e
caiping.shi <caiping.shi@majorbio.com>
2017-09-21 13:31:45 +0800

    修改差异表达导表函数

--- src/mbio/workflows/ref_rna/refrna.py
+++ src/mbio/workflows/ref_rna/refrna.py
@@ -1417,7 +1417,7 @@ class RefrnaWorkflow(Workflow):
                 con_exp = f.split('_edgr_stat.xls')[0].split('_vs_')
                 name = con_exp[0]
                 compare_name = con_exp[1]
-                self.api_exp.add_express_diff_detail(express_diff_id=diff_express_id,
+                self.api.refrna_express.add_express_diff_detail(express_diff_id=diff_express_id,
                                              name=name,
                                              compare_name=compare_name,
                                              ref_all='ref',
@@ -1427,7 +1427,7 @@ class RefrnaWorkflow(Workflow):
                                              query_type="transcript",
                                              pvalue_padjust=params["pvalue_padjust"])
         """添加summary表"""
-        self.api_exp.add_diff_summary_detail(diff_express_id=diff_express_id,
+        self.api.refrna_express.add_diff_summary_detail(diff_express_id=diff_express_id,
                                              count_path=merge_path,
                                              ref_all='ref',
                                              query_type="transcript",
@@ -1490,7 +1490,7 @@ class RefrnaWorkflow(Workflow):
                 con_exp = f.split('_edgr_stat.xls')[0].split('_vs_')
                 name = con_exp[0]
                 compare_name = con_exp[1]
-                self.api_exp.add_express_diff_detail(express_diff_id=diff_express_id,
+                self.api.refrna_express.add_express_diff_detail(express_diff_id=diff_express_id,
                                              name=name,
                                              compare_name=compare_name,
                                              ref_all='ref',
@@ -1500,7 +1500,7 @@ class RefrnaWorkflow(Workflow):
                                              query_type="gene",
                                              pvalue_padjust=params["pvalue_padjust"])
         """添加summary表"""
-        self.api_exp.add_diff_summary_detail(diff_express_id=diff_express_id,
+        self.api.refrna_express.add_diff_summary_detail(diff_express_id=diff_express_id,
                                              count_path=merge_path,
                                              ref_all='ref',
                                              query_type="gene",
