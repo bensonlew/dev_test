d44f976a6a211b9be76ef7b3ce98f4683c5a12cb
曾静 <jing.zeng@majorbio.local>
2017-09-27 09:24:24 +0800

    提交饼图导表

--- src/mbio/api/database/toolapps/tools/graph/simple_pie.py
+++ src/mbio/api/database/toolapps/tools/graph/simple_pie.py
@@ -13,7 +13,7 @@ class SimplePie(Base):
     def __init__(self, bind_object):
         super(SimplePie, self).__init__(bind_object)
         self.output_dir = self.bind_object.output_dir
-        self.work_dir = self.bind_object.work_dir
+        self.work_dir = self.bind_object._task.work_dir
         if Config().MONGODB == 'sanger':
             self._db_name = 'toolapps'
         else:
@@ -25,63 +25,77 @@ class SimplePie(Base):
         """
         运行函数
         """
-        self.main_id = self.simple_pie_in()
-        self.table_ids = self.table_in()
+        if self.bind_object._task.option("group_table").is_set and self.bind_object._task.option("calculation") != 'none':
+            for i in self.bind_object._task.option("group_table").prop['group_scheme']:
+                self.main_id = self.simple_pie_in(i)
+                self.table_ids = self.table_in(i)
+        else:
+            self.main_id = self.simple_pie_in()
+            self.table_ids = self.table_in()
         return self.main_id
         pass
 
-    def simple_pie_in(self):
+    def simple_pie_in(self, group=None):
         """
         导入simple_pie图相关信息
         """
+        self.bind_object.logger.info("开始饼图导表")
+        if self.bind_object._task.option("group_table").is_set and self.bind_object._task.option("calculation") != 'none':
+            pie = self.insert_pie(self.output_dir + '/' + group + '_matrix_pie.xls', 'pie', '一个样本多个值的饼图表格', group)
 
-        all_file = os.listdir(self.output_dir)
-        for fr in all_file:
-            if fr.endswith("matrix_pie.xls"):
-                files = os.path.join(self.output_dir, fr)
-                with open(files) as f:
-                    simple_pie_id = self.db['pie'].insert_one(SON(
-                        project_sn=self.bind_object.sheet.project_sn,
-                        task_id=self.bind_object.id,
-                        name='pie',
-                        desc='一个样本多个值的饼图表格',
-                        status='faild',
-                        created_ts=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    )).inserted_id
-                    lines = f.readlines()
-                    lines = [line for line in lines if (line != "\r\n") and (line != "\n")]
-                    lines = [line for line in lines if not re.search(r"^(\s*\t+?)\s*\t*\n*", line)]
-                    first_line = lines[0].strip().split("\t")
-                    xAxis = []
-                    for i in first_line[1:]:
-                        xAxis.append(i)
-                    samples = []
-                    insert_data = []
-                    for line in lines[1:]:
-                        sample_data = []
-                        line_split = line.strip().split("\t")
-                        samples.append(line_split[0])
-                        for i in range(len(first_line)-1):
-                                dic = dict()
-                                dic["name"] = first_line[i+1]
-                                dic["value"] = float(line_split[i+1])
-                                print dic
-                                sample_data.append(dic)
-                        data = SON(sample_name=line_split[0], pie_id=simple_pie_id, value=sample_data)
-                        insert_data.append(data)
-                    self.db['pie_detail'].insert_many(insert_data)
-                    self.db['pie'].update_one({'_id': simple_pie_id},
-                                                {'$set': {'status': 'end', 'attrs': samples, 'categories': xAxis}})
-                    return simple_pie_id
+        else:
+            pie = self.insert_pie(self.output_dir + '/matrix_pie.xls', 'pie', '一个样本多个值的饼图表格')
+        return [pie]
 
-    def table_in(self):
+    def insert_pie(self, fp, name, desc, group_name=None):
+        with open(fp) as f:
+            simple_pie_id = self.db['pie'].insert_one(SON(
+                project_sn=self.bind_object.sheet.project_sn,
+                task_id=self.bind_object.id,
+                name=name,
+                desc=desc,
+                status='faild',
+                created_ts=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                group_name=group_name,
+            )).inserted_id
+            lines = f.readlines()
+            lines = [line for line in lines if (line != "\r\n") and (line != "\n")]
+            lines = [line for line in lines if not re.search(r"^(\s*\t+?)\s*\t*\n*", line)]
+            first_line = lines[0].strip().split("\t")
+            xAxis = []
+            for i in first_line[1:]:
+                xAxis.append(i)
+            samples = []
+            insert_data = []
+            for line in lines[1:]:
+                sample_data = []
+                line_split = line.strip().split("\t")
+                samples.append(line_split[0])
+                for i in range(len(first_line)-1):
+                        dic = dict()
+                        dic["name"] = first_line[i+1]
+                        dic["value"] = float(line_split[i+1])
+                        sample_data.append(dic)
+                data = SON(sample_name=line_split[0], pie_id=simple_pie_id, value=sample_data)
+                insert_data.append(data)
+            self.db['pie_detail'].insert_many(insert_data)
+            self.db['pie'].update_one({'_id': simple_pie_id},
+                                        {'$set': {'status': 'end', 'attrs': samples, 'categories': xAxis}})
+            return simple_pie_id
+
+    def table_in(self, group=None):
         """
         导入表格相关信息
         """
-        value_table = self.insert_table(self.output_dir + '/final_value.xls', '饼图数据表', '饼图的数据表格')
+        self.bind_object.logger.info("开始表格导表")
+        if self.bind_object._task.option("group_table").is_set and self.bind_object._task.option("calculation") != 'none':
+            value_table = self.insert_table(self.output_dir + '/' + group + '_final_value.xls', '饼图数据表', '饼图的数据表格',
+                                            group)
+        else:
+            value_table = self.insert_table(self.output_dir + '/final_value.xls', '饼图数据表', '饼图的数据表格')
         return [value_table]
 
-    def insert_table(self, fp, name, desc):
+    def insert_table(self, fp, name, desc, group_name=None):
         with open(fp) as f:
             lines = f.readlines()
             columns = lines[0].strip().split("\t")
@@ -94,6 +108,7 @@ class SimplePie(Base):
                 desc=desc,
                 status='end',
                 created_ts=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                group_name=group_name,
             )).inserted_id
         with open(fp) as f:
             lines = f.readlines()
